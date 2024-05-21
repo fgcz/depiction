3063a4a2f72f87eee93bac5b2fe1b6dd957d4ba6
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-05-21 10:31:33 +0200

    doc

--- src/ionmapper/parallel_ops/parallel_config.py
+++ src/ionmapper/parallel_ops/parallel_config.py
@@ -6,22 +6,25 @@ import numpy as np
 
 @dataclass
 class ParallelConfig:
+    """Encapsulates the configuration for parallel processing, and some common functionality to use this information.
+    :param n_jobs: the number of parallel jobs to run
+    :param task_size: the size of each task, if None, the number of tasks will be divided by the number of jobs
+    :param verbose: the verbosity level of the parallel processing (passed to joblib currently)
+    """
+
     n_jobs: int
     task_size: int | None
     verbose: int = 1
 
     def get_splits_count(self, n_items: int) -> int:
-        """
-        Returns the number of necessary splits to process the given number of items.
-        """
+        """Returns the number of necessary splits to process the given number of items."""
         if self.task_size:
             return max(n_items // self.task_size, 1)
         else:
             return min(self.n_jobs, n_items)
 
     def get_task_splits(self, n_items: int | None = None, item_indices: np.ndarray | None = None) -> list[list[int]]:
-        """
-        Returns a list of the indices of the spectra to process in each task.
+        """Returns a list of the indices of the spectra to process in each task.
         Either n_items or item_indices must be provided, but not both.
         :param n_items: the total number of items
         :param item_indices: restrict the operation to the given item's indices, if None, all items are processed
@@ -37,5 +40,4 @@ class ParallelConfig:
     @classmethod
     def no_parallelism(cls) -> ParallelConfig:
         """Returns a ParallelConfig object with no parallelism."""
-        # Note: Maybe uses of this method could also hint at design issues in the code.
         return cls(n_jobs=1, task_size=None, verbose=1)

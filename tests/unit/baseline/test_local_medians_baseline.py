5da182afa0d87addf97edbd5bed2fec4ca93ab9f
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-05-21 13:36:31 +0200

    restructure tests for baseline module

--- tests/unit/baseline/test_local_medians_baseline.py
+++ tests/unit/baseline/test_local_medians_baseline.py
@@ -1,5 +1,6 @@
 from functools import cached_property
-from ionplotter.evaluate_local_medians_baseline import EvaluateLocalMediansBaseline
+
+from ionplotter.baseline import LocalMediansBaseline
 from ionplotter.misc.integration_test_utils import IntegrationTestUtils
 from unittest.mock import MagicMock, patch
 import numpy as np
@@ -8,32 +9,32 @@ import unittest
 
 
 @patch.dict(os.environ, {"NUMBA_DEBUGINFO": "1"})
-class TestEvaluateLocalMediansBaseline(unittest.TestCase):
+class TestLocalMediansBaseline(unittest.TestCase):
     def setUp(self) -> None:
         self.mock_window_unit = "index"
         self.mock_window_size = 5
         IntegrationTestUtils.treat_warnings_as_error(self)
 
     @cached_property
-    def mock_evaluate(self) -> EvaluateLocalMediansBaseline:
-        return EvaluateLocalMediansBaseline(window_size=self.mock_window_size, window_unit=self.mock_window_unit)
+    def mock_baseline(self) -> LocalMediansBaseline:
+        return LocalMediansBaseline(window_size=self.mock_window_size, window_unit=self.mock_window_unit)
 
     def test_evaluate_baseline_when_unit_index(self) -> None:
         mock_mz_arr = MagicMock(name="mock_mz_arr")
         mock_int_arr = np.array([0, 0, 10, 10, 10, 10, 0, 0, 0, 10, 10])
-        int_baseline = self.mock_evaluate.evaluate_baseline(mz_arr=mock_mz_arr, int_arr=mock_int_arr)
+        int_baseline = self.mock_baseline.evaluate_baseline(mz_arr=mock_mz_arr, int_arr=mock_int_arr)
         np.testing.assert_array_equal([10, 10, 10, 10, 10, 10, 0, 0, 0, 0, 0], int_baseline)
 
         # the operation should be symmetric
         mock_int_arr_rev = np.flip(mock_int_arr)
-        int_baseline_rev = self.mock_evaluate.evaluate_baseline(mz_arr=mock_mz_arr, int_arr=mock_int_arr_rev)
+        int_baseline_rev = self.mock_baseline.evaluate_baseline(mz_arr=mock_mz_arr, int_arr=mock_int_arr_rev)
         np.testing.assert_array_equal([0, 0, 0, 0, 0, 10, 10, 10, 10, 10, 10], int_baseline_rev)
 
     def test_evaluate_baseline_when_unit_ppm(self) -> None:
         self.mock_window_unit = "ppm"
         mock_mz_arr = np.linspace(10, 100, 20)
         mock_int_arr = np.ones(20)
-        int_baseline = self.mock_evaluate.evaluate_baseline(mz_arr=mock_mz_arr, int_arr=mock_int_arr)
+        int_baseline = self.mock_baseline.evaluate_baseline(mz_arr=mock_mz_arr, int_arr=mock_int_arr)
         np.testing.assert_array_equal(np.ones(20), int_baseline)
 
     def test_evaluate_baseline_when_unit_ppm_correct_left(self) -> None:
@@ -50,7 +51,7 @@ class TestEvaluateLocalMediansBaseline(unittest.TestCase):
             mz_error = ppm_distance / 1e6 * mock_mz_arr[i - 1]
             mock_mz_arr[i] = mock_mz_arr[i - 1] + mz_error
 
-        int_baseline = self.mock_evaluate.evaluate_baseline(mz_arr=mock_mz_arr, int_arr=mock_int_arr)
+        int_baseline = self.mock_baseline.evaluate_baseline(mz_arr=mock_mz_arr, int_arr=mock_int_arr)
         expected_arr = np.ones(n_values)
         expected_arr[:3] = 0
         np.testing.assert_array_equal(expected_arr, int_baseline)
@@ -69,17 +70,17 @@ class TestEvaluateLocalMediansBaseline(unittest.TestCase):
             mz_error = ppm_distance / 1e6 * mock_mz_arr[i - 1]
             mock_mz_arr[i] = mock_mz_arr[i - 1] + mz_error
 
-        int_baseline = self.mock_evaluate.evaluate_baseline(mz_arr=mock_mz_arr, int_arr=mock_int_arr)
+        int_baseline = self.mock_baseline.evaluate_baseline(mz_arr=mock_mz_arr, int_arr=mock_int_arr)
         expected_arr = np.ones(n_values)
         expected_arr[-3:] = 0
         np.testing.assert_array_equal(expected_arr, int_baseline)
 
-    @patch.object(EvaluateLocalMediansBaseline, "evaluate_baseline")
+    @patch.object(LocalMediansBaseline, "evaluate_baseline")
     def test_subtract_baseline(self, method_evaluate_baseline) -> None:
         method_evaluate_baseline.return_value = np.array([20, 20, 30, 30, 30])
         mock_int_arr = np.array([50, 10, 10, 10, 50])
         mock_mz_arr = MagicMock(name="mock_mz_arr")
-        int_arr = self.mock_evaluate.subtract_baseline(mz_arr=mock_mz_arr, int_arr=mock_int_arr)
+        int_arr = self.mock_baseline.subtract_baseline(mz_arr=mock_mz_arr, int_arr=mock_int_arr)
         np.testing.assert_array_equal([30, 0, 0, 0, 20], int_arr)
 
 
--- tests/unit/baseline/test_tophat_baseline.py
+++ tests/unit/baseline/test_tophat_baseline.py
@@ -1,24 +1,26 @@
-from functools import cached_property
-from ionplotter.evaluate_tophat_baseline import EvaluateTophatBaseline
-from ionplotter.misc.integration_test_utils import IntegrationTestUtils
-from unittest.mock import patch, MagicMock
-import ionplotter.evaluate_tophat_baseline as test_module
-import numpy as np
 import os
 import unittest
+from functools import cached_property
+from unittest.mock import patch, MagicMock
+
+import numpy as np
+
+import ionplotter.baseline.tophat_baseline as test_module
+from ionplotter.baseline import TophatBaseline
+from ionplotter.misc.integration_test_utils import IntegrationTestUtils
 
 
 @patch.dict(os.environ, {"NUMBA_DEBUGINFO": "1"})
-class TestEvaluateTophatBaseline(unittest.TestCase):
+class TestTophatBaseline(unittest.TestCase):
     def setUp(self) -> None:
         self.mock_window_size = 5
         self.mock_window_unit = "index"
         IntegrationTestUtils.treat_warnings_as_error(self)
 
     @cached_property
-    def mock_evaluate(self) -> EvaluateTophatBaseline:
+    def mock_baseline(self) -> TophatBaseline:
         # noinspection PyTypeChecker
-        return EvaluateTophatBaseline(window_size=self.mock_window_size, window_unit=self.mock_window_unit)
+        return TophatBaseline(window_size=self.mock_window_size, window_unit=self.mock_window_unit)
 
     def test_compute_erosion(self) -> None:
         x = np.array([10, 20, 30, 40, 50, 60, 70, 80, 90, 100], dtype=float)
@@ -58,32 +60,32 @@ class TestEvaluateTophatBaseline(unittest.TestCase):
     def test_evaluate_baseline(self) -> None:
         mock_mz_arr = MagicMock(name="mock_mz_arr")
         x = np.array([10, 20, 30, 10, 10, 10, 20, 10, 10, 10, 10, 10, 10, 10, 50, 10, 10])
-        baseline = self.mock_evaluate.evaluate_baseline(mock_mz_arr, x)
+        baseline = self.mock_baseline.evaluate_baseline(mock_mz_arr, x)
         np.testing.assert_array_equal(np.full_like(x, 10.0), baseline)
 
-    @patch.object(EvaluateTophatBaseline, "evaluate_baseline")
+    @patch.object(TophatBaseline, "evaluate_baseline")
     def test_subtract_baseline(self, method_evaluate_baseline) -> None:
         method_evaluate_baseline.return_value = np.array([20, 20, 30, 30, 30])
         mock_int_arr = np.array([50, 10, 10, 10, 50])
         mock_mz_arr = MagicMock(name="mock_mz_arr")
-        int_arr = self.mock_evaluate.subtract_baseline(mz_arr=mock_mz_arr, int_arr=mock_int_arr)
+        int_arr = self.mock_baseline.subtract_baseline(mz_arr=mock_mz_arr, int_arr=mock_int_arr)
         np.testing.assert_array_equal([30, 0, 0, 0, 20], int_arr)
 
     def test_get_element_size_when_index(self) -> None:
         mock_mz_arr = MagicMock(name="mock_mz_arr", spec=[])
-        self.assertEqual(5, self.mock_evaluate.get_element_size(mz_arr=mock_mz_arr))
+        self.assertEqual(5, self.mock_baseline.get_element_size(mz_arr=mock_mz_arr))
 
     def test_get_element_size_when_ppm(self) -> None:
         self.mock_window_unit = "ppm"
         self.mock_window_size = 500
         mock_mz_arr = np.arange(995, 1005, 0.05)
-        self.assertEqual(10, self.mock_evaluate.get_element_size(mz_arr=mock_mz_arr))
+        self.assertEqual(10, self.mock_baseline.get_element_size(mz_arr=mock_mz_arr))
 
     def test_get_element_size_when_invalid(self) -> None:
         self.mock_window_unit = "mz"
         mock_mz_arr = MagicMock(name="mock_mz_arr", spec=[])
         with self.assertRaises(ValueError):
-            self.mock_evaluate.get_element_size(mz_arr=mock_mz_arr)
+            self.mock_baseline.get_element_size(mz_arr=mock_mz_arr)
 
 
 if __name__ == "__main__":

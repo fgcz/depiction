b92eb000872e54a0f23deda7aa086aa847653a89
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-05-21 13:43:52 +0200

    docstring

--- src/ionplotter/baseline/baseline.py
+++ src/ionplotter/baseline/baseline.py
@@ -5,9 +5,13 @@ from numpy.typing import NDArray
 
 
 class Baseline(Protocol):
+    """Baseline estimation and subtraction protocol."""
+
     def evaluate_baseline(self, mz_arr: NDArray[float], int_arr: NDArray[float]) -> NDArray[float]:
-        pass
+        """Returns the spectrum intensities of the baseline. """
+        raise NotImplementedError()
 
     def subtract_baseline(self, mz_arr: NDArray[float], int_arr: NDArray[float]) -> NDArray[float]:
+        """Returns the spectrum intensities with the baseline subtracted."""
         baseline = self.evaluate_baseline(mz_arr=mz_arr, int_arr=int_arr)
         return np.maximum(int_arr - baseline, 0)

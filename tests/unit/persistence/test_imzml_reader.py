4e84533aac37523227ec1cdb62abe4f6b3cc094b
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-05-22 16:15:48 +0200

    fmt

--- tests/unit/persistence/test_imzml_reader.py
+++ tests/unit/persistence/test_imzml_reader.py
@@ -14,10 +14,12 @@ class TestImzmlReader(unittest.TestCase):
     def setUp(self) -> None:
         self.mock_mz_arr_list = [[100, 200], [100, 150, 200], [101, 201]]
         self.mock_int_arr_list = [[1.0, 2.0], [3.0, 4.0, 5.0], [6.0, 7.0]]
-        self.mock_mz_arr_offsets, self.mock_mz_arr_lengths = self._generate_offsets_lengths(self.mock_mz_arr_list,
-                                                                                            n_bytes=4)
-        self.mock_int_arr_offsets, self.mock_int_arr_lengths = self._generate_offsets_lengths(self.mock_int_arr_list,
-                                                                                              n_bytes=4)
+        self.mock_mz_arr_offsets, self.mock_mz_arr_lengths = self._generate_offsets_lengths(
+            self.mock_mz_arr_list, n_bytes=4
+        )
+        self.mock_int_arr_offsets, self.mock_int_arr_lengths = self._generate_offsets_lengths(
+            self.mock_int_arr_list, n_bytes=4
+        )
         self.mock_int_arr_offsets = [offset + 100 for offset in self.mock_int_arr_offsets]
         self.mock_int_arr_dtype = "f"
         self.mock_mz_arr_dtype = "i"
@@ -27,10 +29,16 @@ class TestImzmlReader(unittest.TestCase):
 
     @cached_property
     def mock_reader(self) -> ImzmlReader:
-        return ImzmlReader(mz_arr_offsets=self.mock_mz_arr_offsets, mz_arr_lengths=self.mock_mz_arr_lengths,
-                           mz_arr_dtype=self.mock_mz_arr_dtype, int_arr_offsets=self.mock_int_arr_offsets,
-                           int_arr_lengths=self.mock_int_arr_lengths, int_arr_dtype=self.mock_int_arr_dtype,
-                           coordinates=self.mock_coordinates, imzml_path=self.mock_imzml_path, )
+        return ImzmlReader(
+            mz_arr_offsets=self.mock_mz_arr_offsets,
+            mz_arr_lengths=self.mock_mz_arr_lengths,
+            mz_arr_dtype=self.mock_mz_arr_dtype,
+            int_arr_offsets=self.mock_int_arr_offsets,
+            int_arr_lengths=self.mock_int_arr_lengths,
+            int_arr_dtype=self.mock_int_arr_dtype,
+            coordinates=self.mock_coordinates,
+            imzml_path=self.mock_imzml_path,
+        )
 
     def _generate_offsets_lengths(self, arr_list, n_bytes: int):
         offsets = [0]
@@ -70,15 +78,13 @@ class TestImzmlReader(unittest.TestCase):
         result = self.mock_reader.ibd_mmap
         self.assertEqual(mock_mmap.return_value, result)
         mock_mmap.assert_called_once_with(
-            fileno=mock_ibd_path.open.return_value.fileno.return_value,
-            length=0,
-            access=mmap.ACCESS_READ
+            fileno=mock_ibd_path.open.return_value.fileno.return_value, length=0, access=mmap.ACCESS_READ
         )
         mock_ibd_path.open.assert_called_once_with("rb")
         mock_ibd_path.open.return_value.fileno.assert_called_once_with()
         self.assertEqual(mock_ibd_path.open.return_value, self.mock_reader._ibd_file)
 
-    def test_ibd_mmap_when_present(self):
+    def test_ibd_mmap_when_present(self) -> None:
         mock_mmap = MagicMock(name="mock_mmap")
         self.mock_reader._ibd_mmap = mock_mmap
         self.assertEqual(mock_mmap, self.mock_reader.ibd_mmap)
@@ -93,8 +99,9 @@ class TestImzmlReader(unittest.TestCase):
 
     def test_close(self) -> None:
         with (
-            patch.object(self.mock_reader, "_ibd_mmap") as mock_ibd_mmap, patch.object(self.mock_reader,
-                                                                                       "_ibd_file") as mock_ibd_file, ):
+            patch.object(self.mock_reader, "_ibd_mmap") as mock_ibd_mmap,
+            patch.object(self.mock_reader, "_ibd_file") as mock_ibd_file,
+        ):
             self.mock_reader.close()
         mock_ibd_mmap.close.assert_called_once_with()
         mock_ibd_file.close.assert_called_once_with()
@@ -104,29 +111,29 @@ class TestImzmlReader(unittest.TestCase):
             # main check is that this does not raise
             self.mock_reader.close()
 
-    def test_imzml_mode_when_continuous(self):
+    def test_imzml_mode_when_continuous(self) -> None:
         self.mock_mz_arr_offsets = [7, 7, 7]
         self.assertEqual(ImzmlModeEnum.CONTINUOUS, self.mock_reader.imzml_mode)
 
-    def test_imzml_mode_when_processed(self):
+    def test_imzml_mode_when_processed(self) -> None:
         self.assertEqual(ImzmlModeEnum.PROCESSED, self.mock_reader.imzml_mode)
 
     def test_n_spectra(self) -> None:
         self.assertEqual(3, self.mock_reader.n_spectra)
 
-    def test_coordinates(self):
+    def test_coordinates(self) -> None:
         self.mock_coordinates = np.array([(1, 2, 3), (4, 5, 6)])
         expected = np.array([[1, 2, 3], [4, 5, 6]], dtype=int)
         np.testing.assert_array_equal(expected, self.mock_reader.coordinates)
 
-    def test_coordinates_2d(self):
+    def test_coordinates_2d(self) -> None:
         self.mock_coordinates = np.array([(1, 2, 3), (4, 5, 6)])
         expected = np.array([[1, 2], [4, 5]], dtype=int)
         np.testing.assert_array_equal(expected, self.mock_reader.coordinates_2d)
 
     @patch.object(ImzmlReader, "get_spectrum_mz")
     @patch.object(ImzmlReader, "get_spectrum_int")
-    def test_get_spectrum(self, mock_get_spectrum_int, mock_get_spectrum_mz):
+    def test_get_spectrum(self, mock_get_spectrum_int, mock_get_spectrum_mz) -> None:
         mock_i_spectrum = MagicMock(name="mock_i_spectrum", spec=[])
         mz_arr, int_arr = self.mock_reader.get_spectrum(i_spectrum=mock_i_spectrum)
         self.assertEqual(mock_get_spectrum_mz.return_value, mz_arr)
@@ -137,7 +144,7 @@ class TestImzmlReader(unittest.TestCase):
     @patch.object(ImzmlReader, "get_spectrum_mz")
     @patch.object(ImzmlReader, "get_spectrum_int")
     @patch.object(ImzmlReader, "get_spectrum_coordinates")
-    def test_get_spectrum_with_coords(self, mock_get_spectrum_coordinates, mock_get_spectrum_int, mock_get_spectrum_mz):
+    def test_get_spectrum_with_coords(self, mock_get_spectrum_coordinates, mock_get_spectrum_int, mock_get_spectrum_mz) -> None:
         mock_i_spectrum = MagicMock(name="mock_i_spectrum", spec=[])
         mz_arr, int_arr, coords = self.mock_reader.get_spectrum_with_coords(i_spectrum=mock_i_spectrum)
         self.assertEqual(mock_get_spectrum_mz.return_value, mz_arr)
@@ -150,71 +157,71 @@ class TestImzmlReader(unittest.TestCase):
     @patch.object(ImzmlReader, "get_spectrum_mz")
     @patch.object(ImzmlReader, "get_spectrum_int")
     @patch.object(ImzmlReader, "imzml_mode", new_callable=PropertyMock)
-    def test_get_spectra_when_continuous(self, prop_imzml_mode, mock_get_spectrum_int, mock_get_spectrum_mz):
+    def test_get_spectra_when_continuous(self, prop_imzml_mode, mock_get_spectrum_int, mock_get_spectrum_mz) -> None:
         prop_imzml_mode.return_value = ImzmlModeEnum.CONTINUOUS
         i_spectra = [2, 4, 9]
-        mock_get_spectrum_mz.return_value = np.array([1., 2])
+        mock_get_spectrum_mz.return_value = np.array([1.0, 2])
         mock_get_spectrum_int.side_effect = [np.array([1, 2]), np.array([3, 4]), np.array([5, 6])]
         mz_arr_list, int_arr_list = self.mock_reader.get_spectra(i_spectra=i_spectra)
-        np.testing.assert_array_equal(np.array([[1., 2], [1, 2], [1, 2]]), mz_arr_list)
+        np.testing.assert_array_equal(np.array([[1.0, 2], [1, 2], [1, 2]]), mz_arr_list)
         mock_get_spectrum_mz.assert_called_once_with(i_spectrum=2)
         expected_int_arr_list = np.array([[1, 2], [3, 4], [5, 6]])
         np.testing.assert_array_equal(expected_int_arr_list, int_arr_list)
-        self.assertListEqual([call(i_spectrum=2), call(i_spectrum=4), call(i_spectrum=9)],
-                             mock_get_spectrum_int.mock_calls)
+        self.assertListEqual(
+            [call(i_spectrum=2), call(i_spectrum=4), call(i_spectrum=9)], mock_get_spectrum_int.mock_calls
+        )
 
     @patch.object(ImzmlReader, "get_spectrum")
     @patch.object(ImzmlReader, "imzml_mode", new_callable=PropertyMock)
-    def test_get_spectra_when_processed(self, prop_imzml_mode, mock_get_spectrum):
+    def test_get_spectra_when_processed(self, prop_imzml_mode, mock_get_spectrum) -> None:
         prop_imzml_mode.return_value = ImzmlModeEnum.PROCESSED
         i_spectra = [2, 4, 9]
-        mock_get_spectrum.side_effect = [(self.mock_mz_arr_list[0], self.mock_int_arr_list[0]),
-                                         (self.mock_mz_arr_list[1], self.mock_int_arr_list[1]),
-                                         (self.mock_mz_arr_list[2], self.mock_int_arr_list[2])]
+        mock_get_spectrum.side_effect = [
+            (self.mock_mz_arr_list[0], self.mock_int_arr_list[0]),
+            (self.mock_mz_arr_list[1], self.mock_int_arr_list[1]),
+            (self.mock_mz_arr_list[2], self.mock_int_arr_list[2]),
+        ]
         mz_arr_list, int_arr_list = self.mock_reader.get_spectra(i_spectra=i_spectra)
         expected_mz_arr_list = [np.array([100, 200]), np.array([100, 150, 200]), np.array([101, 201])]
         expected_int_arr_list = [np.array([1.0, 2.0]), np.array([3.0, 4.0, 5.0]), np.array([6.0, 7.0])]
         np.testing.assert_equal(expected_mz_arr_list, mz_arr_list)
         np.testing.assert_equal(expected_int_arr_list, int_arr_list)
-        self.assertListEqual([call(i_spectrum=2), call(i_spectrum=4), call(i_spectrum=9)],
-                             mock_get_spectrum.mock_calls)
+        self.assertListEqual([call(i_spectrum=2), call(i_spectrum=4), call(i_spectrum=9)], mock_get_spectrum.mock_calls)
 
     @patch.object(ImzmlReader, "ibd_mmap", new_callable=PropertyMock)
-    def test_get_spectrum_mz(self, mock_ibd_mmap):
+    def test_get_spectrum_mz(self, mock_ibd_mmap) -> None:
         self.mock_mz_arr_dtype = "i"
         mock_ibd_mmap.return_value.read.return_value = b"\x01\x00\x00\x00\x02\x00\x00\x00"
         mz_arr = self.mock_reader.get_spectrum_mz(1)
         np.testing.assert_array_equal([1, 2], mz_arr)
         self.assertListEqual(
-            [call.seek(self.mock_mz_arr_offsets[1]),
-             call.read(self.mock_mz_arr_lengths[1] * 4)],
+            [call.seek(self.mock_mz_arr_offsets[1]), call.read(self.mock_mz_arr_lengths[1] * 4)],
             mock_ibd_mmap.return_value.mock_calls,
         )
 
     @patch.object(ImzmlReader, "ibd_mmap", new_callable=PropertyMock)
-    def test_get_spectrum_int(self, mock_ibd_mmap):
+    def test_get_spectrum_int(self, mock_ibd_mmap) -> None:
         self.mock_int_arr_dtype = "i"
         mock_ibd_mmap.return_value.read.return_value = b"\x02\x00\x00\x00\x03\x00\x00\x00\x04\x00\x00\x00"
         mz_arr = self.mock_reader.get_spectrum_int(1)
         np.testing.assert_array_equal([2, 3, 4], mz_arr)
         self.assertListEqual(
-            [call.seek(self.mock_int_arr_offsets[1]),
-             call.read(self.mock_int_arr_lengths[1] * 4)],
+            [call.seek(self.mock_int_arr_offsets[1]), call.read(self.mock_int_arr_lengths[1] * 4)],
             mock_ibd_mmap.return_value.mock_calls,
         )
 
-    def test_get_spectrum_coordinates(self):
+    def test_get_spectrum_coordinates(self) -> None:
         self.mock_coordinates = np.array([(1, 2, 3), (4, 5, 6)])
         np.testing.assert_array_equal([1, 2, 3], self.mock_reader.get_spectrum_coordinates(0))
         np.testing.assert_array_equal([4, 5, 6], self.mock_reader.get_spectrum_coordinates(1))
 
-    def test_get_spectrum_n_points(self):
+    def test_get_spectrum_n_points(self) -> None:
         self.assertEqual(2, self.mock_reader.get_spectrum_n_points(0))
         self.assertEqual(3, self.mock_reader.get_spectrum_n_points(1))
         self.assertEqual(2, self.mock_reader.get_spectrum_n_points(2))
 
     @patch.object(ImzmlReader, "get_spectrum_mz")
-    def test_get_spectra_mz_range_when_specified(self, mock_get_spectrum_mz):
+    def test_get_spectra_mz_range_when_specified(self, mock_get_spectrum_mz) -> None:
         mock_i_spectra = [1, 3, 5]
         mock_get_spectrum_mz.side_effect = [np.array([1, 2]), np.array([0.5, 4]), np.array([5, 6])]
         mz_range = self.mock_reader.get_spectra_mz_range(i_spectra=mock_i_spectra)
@@ -231,10 +238,12 @@ class TestImzmlReader(unittest.TestCase):
         self.assertListEqual([call(0), call(1), call(2)], mock_get_spectrum_mz.mock_calls)
 
     @patch("depiction.persistence.imzml_reader.pyimzml.ImzMLParser.ImzMLParser")
-    def test_parse_imzml(self, module_imzml_parser):
+    def test_parse_imzml(self, module_imzml_parser) -> None:
         mock_path = MagicMock(name="mock_path", spec=[])
         mock_portable_spectrum_reader = MagicMock(name="mock_portable_spectrum_reader")
-        module_imzml_parser.return_value.__enter__.return_value.portable_spectrum_reader.return_value = mock_portable_spectrum_reader
+        module_imzml_parser.return_value.__enter__.return_value.portable_spectrum_reader.return_value = (
+            mock_portable_spectrum_reader
+        )
         mock_portable_spectrum_reader.mzPrecision = "f"
         mock_portable_spectrum_reader.intensityPrecision = "i"
         result = ImzmlReader.parse_imzml(path=mock_path)
@@ -248,8 +257,9 @@ class TestImzmlReader(unittest.TestCase):
         np.testing.assert_array_equal(mock_portable_spectrum_reader.coordinates, result._coordinates)
 
     def test_str(self) -> None:
-        self.assertEqual("ImzmlReader[/dev/null/test.imzML, n_spectra=3, int_arr_dtype=f, mz_arr_dtype=i]",
-                         str(self.mock_reader))
+        self.assertEqual(
+            "ImzmlReader[/dev/null/test.imzML, n_spectra=3, int_arr_dtype=f, mz_arr_dtype=i]", str(self.mock_reader)
+        )
 
 
 if __name__ == "__main__":

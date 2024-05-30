98abb95819fc9d7c4548c56acf4837d74e0c73f1
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-05-30 09:43:26 +0200

    make example reasonable

--- examples/spectra_calibration.py
+++ examples/spectra_calibration.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-
+import spectrum_utils.spectrum
+import spectrum_utils.plot
 from matplotlib import pyplot as plt
 from numpy.typing import NDArray
 from xarray import DataArray
@@ -15,7 +16,7 @@ class NewCalibrationMethod:
     def apply_spectrum_model(
         self, spectrum_mz_arr: NDArray[float], spectrum_int_arr: NDArray[float], model_coef: DataArray
     ) -> tuple[NDArray[float], NDArray[float]]:
-        pass
+        return spectrum_mz_arr + 0.05, spectrum_int_arr
 
 
 def main() -> None:
@@ -31,15 +32,50 @@ def main() -> None:
         peak_picker.pick_peaks(mz_arr, int_arr) for mz_arr, int_arr in spectra_original
     ]
 
-    calib = NewCalibrationMethod()
-    for mz_arr, int_arr in spectra_picked:
-        features = calib.extract_spectrum_features(mz_arr, int_arr)
-        print(features)
-
-    plt.stem(mz_arr, int_arr)
-    plt.xlim(1030, 1080)
+    # show picker result
+    mz_arr, int_arr = spectra_picked[0]
+    plt.figure()
+    ax = plt.gca()
+    ax.stem(mz_arr, int_arr, basefmt=" ", markerfmt="x", linefmt="C0-")
+    ax.set_xlim(1030, 1080)
     plt.show()
 
+    calib = NewCalibrationMethod()
+    all_features = []
+    print("Extracting features...")
+    for mz_arr, int_arr in spectra_picked:
+        features = calib.extract_spectrum_features(mz_arr, int_arr)
+        assert features.dims == ("c",)
+        print(features)
+        all_features.append(features)
+
+    # apply the models to the spectra
+    print("Applying models...")
+    spectra_calib = []
+    for (mz_arr, int_arr), features in zip(spectra_picked, all_features):
+        mz_arr, int_arr = calib.apply_spectrum_model(spectrum_mz_arr=mz_arr, spectrum_int_arr=int_arr, model_coef=features)
+        spectra_calib.append((mz_arr, int_arr))
+
+    # compare the spectra somehow...
+    print("Plot comparison...")
+
+    mz_arr_original, int_arr_original = spectra_picked[0]
+    mz_arr_calib, int_arr_calib = spectra_calib[0]
+
+    print("shape comparison")
+    print(mz_arr_original.shape)
+    print(mz_arr_calib.shape)
+
+    plt.figure()
+    ax = plt.gca()
+    ax.stem(mz_arr_original, int_arr_original, label="original", basefmt=" ", markerfmt="x", linefmt="C0-")
+    ax.stem(mz_arr_calib, int_arr_calib, label="calibrated", basefmt=" ", markerfmt="x", linefmt="C1-")
+    mass_target = 997.53
+    ax.axvline(mass_target, color="red", label="target")
+    ax.legend()
+    ax.set_xlim(mass_target - 5, mass_target + 5)
+    ax.set_ylim(0, 1)
+    plt.show()
 
 def create_data():
     original_file = "/Users/leo/code/msi/code/msi_targeted_preproc/example/data-work/menzha_20231208_s607930_64074-b20-30928-a/baseline_adj.imzML"

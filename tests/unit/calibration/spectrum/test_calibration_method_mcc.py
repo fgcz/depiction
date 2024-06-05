4389b1a2cd0f3d79ba33b77dc44a0a8b1559343f
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-06-05 15:13:07 +0200

    ensure the placeholder test passes for now

--- tests/unit/calibration/spectrum/test_calibration_method_mcc.py
+++ tests/unit/calibration/spectrum/test_calibration_method_mcc.py
@@ -7,7 +7,7 @@ from depiction.calibration.spectrum.calibration_method_mcc import CalibrationMet
 
 class TestCalibrationMethodMCC(unittest.TestCase):
     def test_get_distances_nearest_when_invalid(self) -> None:
-        mccm = CalibrationMethodMassClusterCenterModel.__init__()
+        mccm = CalibrationMethodMassClusterCenterModel(model_smoothing_activated=False)
 
 
 if __name__ == "__main__":

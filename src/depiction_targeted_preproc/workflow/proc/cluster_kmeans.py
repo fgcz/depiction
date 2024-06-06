98ad54fd603e6b070d2b4df0c8cd1c56fc15a712
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-06-06 17:28:35 +0200

    nicer clustering

--- src/depiction_targeted_preproc/workflow/proc/cluster_kmeans.py
+++ src/depiction_targeted_preproc/workflow/proc/cluster_kmeans.py
@@ -4,18 +4,35 @@ from typing import Annotated
 import numpy as np
 import typer
 import xarray
+from loguru import logger
 from sklearn.cluster import KMeans
 from typer import Option
 
 from depiction.image.multi_channel_image import MultiChannelImage
 
 
+def retain_strongest_signals(data: xarray.DataArray, n_features: int) -> xarray.DataArray:
+    """Retain the N strongest signals in the data."""
+    # compute total signal strength per channel c
+    total_signal_strength = xarray.apply_ufunc(np.fabs, data).sum(dim="i")
+    # sort channels by total signal strength
+    sorted_channels = total_signal_strength.argsort()
+    # retain the N strongest signals
+    strongest_channels = sorted_channels.coords["c"][-n_features:]
+    logger.info(f"Retained channels: {strongest_channels}")
+    return data.sel(c=strongest_channels)
+
+
 def cluster_kmeans(input_netcdf_path: Annotated[Path, Option()], output_netcdf_path: Annotated[Path, Option()]) -> None:
     image = MultiChannelImage.read_hdf5(input_netcdf_path)
-    k = 10
+    # TODO make configurable
+    n_clusters = 10
+    n_features = 30
 
-    kmeans = KMeans(n_clusters=k)
-    clusters = kmeans.fit_predict(image.data_flat.transpose("i", "c").values)
+    reduced_data = retain_strongest_signals(image.data_flat.transpose("i", "c"), n_features)
+
+    kmeans = KMeans(n_clusters=n_clusters)
+    clusters = kmeans.fit_predict(reduced_data.values)
 
     cluster_data = xarray.DataArray(
         clusters, dims=("i",), coords={"i": image.data_flat.coords["i"]}
--- src/depiction_targeted_preproc/workflow/vis/clustering.py
+++ src/depiction_targeted_preproc/workflow/vis/clustering.py
@@ -2,22 +2,20 @@ from pathlib import Path
 from typing import Annotated
 
 import typer
+import xarray
 from matplotlib import pyplot as plt
 from typer import Option
-
-from depiction.image.multi_channel_image import MultiChannelImage
-from depiction.visualize.plot_image import PlotImage
 
 
 def vis_clustering(input_netcdf_path: Annotated[Path, Option()],
                    output_png_path: Annotated[Path, Option()]
                    ) -> None:
-    source_image = MultiChannelImage.read_hdf5(input_netcdf_path)
-    plot_image = PlotImage(source_image)
+    source_image = xarray.open_dataarray(input_netcdf_path)
     fig = plt.figure(figsize=(10, 10))
-    # categorical cmap
-    cmap = "tab20"
-    plot_image.plot_single_channel_image(channel=source_image.channel_names[0], cmap=cmap, ax=fig.add_subplot(111))
+    ax = fig.gca()
+    source_image.plot(cmap="tab10", ax=ax)
+    ax.set_aspect("equal")
+
     fig.tight_layout()
     fig.savefig(output_png_path)
 

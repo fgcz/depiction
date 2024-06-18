8e89455bc5d977b5c2dc2b5556b23b7713dfa7eb
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-06-18 08:38:07 +0200

    make the pipeline run with simulated data

--- src/depiction_targeted_preproc/workflow/rules/rules_simulate.smk
+++ src/depiction_targeted_preproc/workflow/rules/rules_simulate.smk
@@ -3,14 +3,15 @@
 #   -> but then it will require some adjustment in the code that calls it
 
 rule simulate_create_labels:
+    input:
+        config = "{sample}_sim/pipeline_params.yml",
     output:
         image="{sample}_sim/true_labels.hdf5",
         overview_image="{sample}_sim/true_labels_overview.png",
-        config="{sample}_sim/pipeline_params.yml",
     shell:
         "python -m depiction_targeted_preproc.workflow.simulate.create_labels"
         " --output-image-path {output.image} --output-overview-image-path {output.overview_image}"
-        " --config-path {output.config}"
+        " --config-path {input.config}"
 
 
 rule simulate_create_mass_list:
@@ -27,7 +28,7 @@ rule simulate_create_mass_list:
 
 rule simulate_generate_imzml:
     input:
-        image="{sample}_sim/simulated_labels.hdf5",
+        image="{sample}_sim/true_labels.hdf5",
         mass_list="{sample}_sim/mass_list.simulated.csv",
         config="{sample}_sim/pipeline_params.yml",
     output:
--- src/depiction_targeted_preproc/workflow/simulate/generate_imzml.py
+++ src/depiction_targeted_preproc/workflow/simulate/generate_imzml.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from typing import Annotated
 
+import numpy as np
 import polars as pl
 import typer
 from typer import Option
@@ -12,24 +13,24 @@ from depiction_targeted_preproc.pipeline_config.model import PipelineParameters
 
 
 def simulate_generate_imzml(
-    image_path: Annotated[Path, Option()],
-    mass_list_path: Annotated[Path, Option()],
+    input_image_path: Annotated[Path, Option()],
+    input_mass_list_path: Annotated[Path, Option()],
     config_path: Annotated[Path, Option()],
     output_imzml_path: Annotated[Path, Option()],
 ) -> None:
     write_file = ImzmlWriteFile(output_imzml_path, imzml_mode=ImzmlModeEnum.CONTINUOUS)
-    label_image = MultiChannelImage.read_hdf5(image_path)
-    mass_list = pl.read_csv(mass_list_path)
+    label_image = MultiChannelImage.read_hdf5(input_image_path)
+    mass_list = pl.read_csv(input_mass_list_path)
     config = PipelineParameters.parse_yaml(config_path)
 
     mz_arr = SyntheticMSIDataGenerator.get_mz_arr(
-        mass_list.min() - 50, mass_list.max() + 50, config.simulate.bin_width_ppm
+        mass_list["mass"].min() - 50, mass_list["mass"].max() + 50, config.simulate.bin_width_ppm
     )
     gen = SyntheticMSIDataGenerator()
     gen.generate_imzml_for_labels(
         write_file=write_file,
         label_image=label_image,
-        label_masses=mass_list["mass"].to_list(),
+        label_masses=np.asarray(mass_list["mass"]),
         n_isotopes=3,
         mz_arr=mz_arr,
         baseline_max_intensity=1.0,

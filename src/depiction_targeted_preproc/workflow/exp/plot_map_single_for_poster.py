3b795a255da9a3f7ad95ebaa3c4b012af2dc2438
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-06-20 08:42:20 +0200

    update (but probably to be deleted anyhow)

--- src/depiction_targeted_preproc/workflow/exp/plot_map_single_for_poster.py
+++ src/depiction_targeted_preproc/workflow/exp/plot_map_single_for_poster.py
@@ -1,27 +1,27 @@
 from pathlib import Path
 from typing import Annotated
-import xarray
+
 import typer
+import xarray
 from matplotlib import pyplot as plt
 
 
-def exp_plot_map_comparison(
-    input_mass_shift_paths: Annotated[list[Path], typer.Argument()],
+def exp_plot_map_single_for_poster(
+    input_mass_shift_path: Annotated[Path, typer.Option()],
     output_pdf_path: Annotated[Path, typer.Option()],
 ) -> None:
     # load all the inputs
-    mass_shifts = [xarray.open_dataarray(path) for path in input_mass_shift_paths]
+    shift_map = xarray.open_dataarray(input_mass_shift_path)
 
-    fig, axs = plt.subplots(1, len(mass_shifts), figsize=(10 * len(mass_shifts), 10))
-    for i, shift_map in enumerate(mass_shifts):
-        shift_map.isel(c=0).plot(x="x", y="y", ax=axs[i], cmap="coolwarm", vmin=-1, vmax=+1)
-        axs[i].set_aspect("equal")
-        variant = input_mass_shift_paths[i].parent.name
-        test_mass = shift_map.coords["c"][0]
-        axs[i].set_title(f"{variant} computed shift for test mass {test_mass:.2f}")
+    plt.figure(figsize=(10, 10))
+    shift_map.isel(c=0).plot(x="x", y="y", ax=plt.gca(), cmap="coolwarm", vmin=-1, vmax=+1, yincrease=False)
+    plt.set_aspect("equal")
+    variant = input_mass_shift_path.parent.name
+    test_mass = shift_map.coords["c"][0]
+    plt.set_title(f"{variant} computed shift for test mass {test_mass:.2f}")
 
     plt.savefig(output_pdf_path, bbox_inches="tight")
 
 
 if __name__ == "__main__":
-    typer.run(exp_plot_map_comparison)
+    typer.run(exp_plot_map_single_for_poster)

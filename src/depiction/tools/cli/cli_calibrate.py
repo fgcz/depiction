fd934885d11e70d0c6e7b626b300c93acd979800
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-07-04 15:29:04 +0200

    add run-global-constant-shift as this a simple and common case

--- src/depiction/tools/cli/cli_calibrate.py
+++ src/depiction/tools/cli/cli_calibrate.py
@@ -6,7 +6,7 @@ import cyclopts
 import yaml
 
 from depiction.persistence import ImzmlReadFile, ImzmlWriteFile, ImzmlModeEnum
-from depiction.tools.calibrate import CalibrationConfig, calibrate
+from depiction.tools.calibrate import CalibrationConfig, calibrate, CalibrationConstantGlobalShiftConfig
 
 app = cyclopts.App()
 
@@ -29,6 +29,30 @@ def run_config(
         output_file=output_file,
         mass_list=input_mass_list,
         coefficient_output_path=output_calib_data,
+    )
+
+
+@app.command
+def run_global_constant_shift(
+    input_imzml: Path,
+    output_imzml: Path,
+    *,
+    input_mass_list: Path | None = None,
+    n_jobs: int | None = None,
+) -> None:
+    config = CalibrationConfig.validate(
+        dict(
+            method=CalibrationConstantGlobalShiftConfig(),
+            n_jobs=n_jobs,
+        )
+    )
+    input_file = ImzmlReadFile(input_imzml)
+    output_file = ImzmlWriteFile(output_imzml, imzml_mode=ImzmlModeEnum.PROCESSED)
+    calibrate(
+        config=config,
+        input_file=input_file,
+        output_file=output_file,
+        mass_list=input_mass_list,
     )
 
 

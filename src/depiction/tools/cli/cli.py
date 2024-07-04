185c839ed9ef2d542edbd126b7317b069da2ab1c
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-07-04 16:02:53 +0200

    add cli entrypoint that dispatches the different commands

---
+++ src/depiction/tools/cli/cli.py
@@ -0,0 +1,15 @@
+from cyclopts import App
+
+from depiction.tools.cli.cli_calibrate import app as calibrate
+from depiction.tools.cli.cli_correct_baseline import app as correct_baseline
+from depiction.tools.cli.cli_filter_peaks import app as filter_peaks
+from depiction.tools.cli.cli_pick_peaks import app as pick_peaks
+
+app = App()
+app.command(calibrate, name="calibrate")
+app.command(correct_baseline, name="correct-baseline")
+app.command(filter_peaks, name="filter-peaks")
+app.command(pick_peaks, name="pick-peaks")
+
+if __name__ == "__main__":
+    app()

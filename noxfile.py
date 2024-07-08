ed2a6f12d4c1c12cea520662165ce0fdd9ecdc63
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-07-08 17:02:24 +0200

    add lint to noxfile

--- noxfile.py
+++ noxfile.py
@@ -3,6 +3,15 @@ import nox
 nox.options.default_venv_backend = "uv"
 
 
+@nox.session(reuse_venv=True)
+def lint(session: nox.Session) -> None:
+    """
+    Run the linter.
+    """
+    session.install("pre-commit")
+    session.run("pre-commit", "run", "--all-files", "--show-diff-on-failure", *session.posargs)
+
+
 @nox.session
 def tests(session):
     session.install(".[testing]")

82c4f521519fe3eb9130aa80f5c8fbe1d28b4114
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-07-08 09:24:01 +0200

    start splitting into functions for testing

--- src/depiction_targeted_preproc/workflow/snakemake_invoke.py
+++ src/depiction_targeted_preproc/workflow/snakemake_invoke.py
@@ -14,21 +14,18 @@ class SnakemakeInvoke:
     use_subprocess: bool = True
     continue_on_error: bool = False
     report_file: str | None = "report.html"
+    n_cores: int = 1
+    env_variables: dict[str, str] | None = None
 
-    def invoke(
-        self, work_dir: Path, result_files: list[Path], *, n_cores: int = 1, env_variables: dict[str, str] | None = None
-    ) -> None:
+    def invoke(self, work_dir: Path, result_files: list[Path]) -> None:
         """Invokes the snakemake workflow to generate the requested result files.
         :param work_dir: The working directory where the data folder structure is located.
         :param result_files: The list of result files to generate (relative to `work_dir`).
-        :param n_cores: The number of cores to use for the workflow execution.
-        :param env_variables: Environment variables to set for the workflow execution.
         """
-        env_variables = env_variables or {}
         if self.use_subprocess:
-            self._invoke_subprocess(work_dir, result_files, n_cores=n_cores, env_variables=env_variables)
+            self._invoke_subprocess(work_dir, result_files)
         else:
-            self._invoke_direct(work_dir, result_files, n_cores=n_cores, env_variables=env_variables)
+            self._invoke_direct(work_dir, result_files)
 
     @property
     def snakefile_path(self) -> Path:
@@ -38,9 +35,7 @@ class SnakemakeInvoke:
     def workflow_dir(self) -> Path:
         return Path(__file__).parents[1] / "workflow"
 
-    def _invoke_direct(
-        self, work_dir: Path, result_files: list[Path], n_cores: int, env_variables: dict[str, str]
-    ) -> None:
+    def _invoke_direct(self, work_dir: Path, result_files: list[Path]) -> None:
         from snakemake.api import SnakemakeApi
         from snakemake.settings import OutputSettings, StorageSettings, ResourceSettings, DAGSettings, ExecutionSettings
 
@@ -52,28 +47,24 @@ class SnakemakeInvoke:
         ) as snakemake_api:
             workflow_api = snakemake_api.workflow(
                 storage_settings=StorageSettings(),
-                resource_settings=ResourceSettings(cores=n_cores),
+                resource_settings=ResourceSettings(cores=self.n_cores),
                 snakefile=self.snakefile_path,
                 workdir=work_dir,
             )
             dag_api = workflow_api.dag(
                 dag_settings=DAGSettings(targets=[str(p) for p in result_files], force_incomplete=True)
             )
-            with self._set_env_vars(env_variables):
+            with self._set_env_vars():
                 dag_api.execute_workflow(execution_settings=ExecutionSettings(keep_going=self.continue_on_error))
 
-    def _invoke_subprocess(
-        self, work_dir: Path, result_files: list[Path], n_cores: int, env_variables: dict[str, str]
-    ) -> None:
+    def _invoke_subprocess(self, work_dir: Path, result_files: list[Path]) -> None:
         snakemake_bin = shutil.which("snakemake")
         if snakemake_bin is None:
             raise RuntimeError(f"snakemake not found, check PATH: {os.environ['PATH']}")
         extra_args = []
         if self.continue_on_error:
             extra_args.append("--keep-going")
-        base_command = self.get_base_command(
-            extra_args=extra_args, n_cores=n_cores, snakemake_bin=snakemake_bin, work_dir=work_dir
-        )
+        base_command = self.get_base_command(extra_args=extra_args, snakemake_bin=snakemake_bin, work_dir=work_dir)
         command = self.get_command_create_results(
             base_command=base_command, result_files=result_files, work_dir=work_dir
         )
@@ -82,7 +73,7 @@ class SnakemakeInvoke:
             command,
             cwd=self.workflow_dir,
             check=True,
-            env={**os.environ, **env_variables},
+            env={**os.environ, **(self.env_variables or {})},
         )
         if self.report_file:
             command = self.get_command_create_report(
@@ -93,7 +84,7 @@ class SnakemakeInvoke:
                 command,
                 cwd=self.workflow_dir,
                 check=True,
-                env={**os.environ, **env_variables},
+                env={**os.environ, **(self.env_variables or {})},
             )
 
     def get_base_command(self, extra_args: list[str], n_cores: int, snakemake_bin: str, work_dir: Path) -> list[str]:
@@ -127,10 +118,10 @@ class SnakemakeInvoke:
         ]
 
     @contextlib.contextmanager
-    def _set_env_vars(self, env_variables: dict[str, str]) -> None:
-        """Temporarily sets the given environment variables for the duration of the context."""
+    def _set_env_vars(self) -> None:
+        """Temporarily sets the configured environment variables for the duration of the context."""
         old_env = os.environ.copy()
-        os.environ.update(env_variables)
+        os.environ.update(self.env_variables or {})
         try:
             yield
         finally:

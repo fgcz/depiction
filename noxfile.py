303acdfbdb1ce43e399819d41786c51cb4a4a80d
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-07-09 10:34:57 +0200

    docstrings

--- noxfile.py
+++ noxfile.py
@@ -5,14 +5,13 @@ nox.options.default_venv_backend = "uv"
 
 @nox.session(reuse_venv=True)
 def lint(session: nox.Session) -> None:
-    """
-    Run the linter.
-    """
+    """Runs the linter."""
     session.install("pre-commit")
     session.run("pre-commit", "run", "--all-files", "--show-diff-on-failure", *session.posargs)
 
 
 @nox.session
-def tests(session):
+def tests(session) -> None:
+    """Runs the test suite."""
     session.install(".[testing]")
     session.run("pytest")

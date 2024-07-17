e1079f8844d7b4b105c1ef1aaf458a11cd8931d0
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-07-17 14:59:56 +0200

    add doc folder

---
+++ doc/source/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "depiction"
+copyright = "2024, Leonardo Schwarz"
+author = "Leonardo Schwarz"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- API
+extensions.append("autoapi.extension")
+autoapi_dirs = ["../../src"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = ["_static"]

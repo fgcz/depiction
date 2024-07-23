470bbb2c02f787ca825fc3107ba7fb01d9122d93
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-07-23 16:34:47 +0200

    add a second plot showing image ids alongside clusters in umap

--- src/depiction_cluster_sandbox/workflow/proc/render_umap_png.py
+++ src/depiction_cluster_sandbox/workflow/proc/render_umap_png.py
@@ -14,10 +14,17 @@ def render_umap_png(input_hdf5: Path, output_png: Path) -> None:
 
     # TODO use UMAP(random_state=...) but this will prevent parallelization
     umap = UMAP().fit_transform(image.data_flat.drop_sel(c="cluster").values.T)
-    plt.figure()
-    ax = plt.gca()
-    ax.scatter(umap[:, 0], umap[:, 1], c=image.data_flat.sel(c="cluster").values.ravel(), s=1, cmap="tab10")
-    ax.axis("off")
+
+    fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+
+    axs[0].scatter(umap[:, 0], umap[:, 1], c=image.data_flat.sel(c="cluster").values.ravel(), s=1, cmap="tab10")
+    axs[0].axis("off")
+    axs[0].set_title("Clusters")
+
+    axs[1].scatter(umap[:, 0], umap[:, 1], c=image.data_flat.sel(c="image_index").values.ravel(), s=1, cmap="tab10")
+    axs[1].axis("off")
+    axs[1].set_title("Image IDs")
+
     plt.savefig(output_png, bbox_inches="tight", pad_inches=0)
 
 

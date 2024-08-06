0481293e667b556a632d68c2bc739018cdec5736
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-08-06 17:07:06 +0200

    use the new concatenation wrapper

--- src/depiction_cluster_sandbox/workflow/proc/concatenate_images.py
+++ src/depiction_cluster_sandbox/workflow/proc/concatenate_images.py
@@ -1,7 +1,9 @@
 from pathlib import Path
+
 import cyclopts
-from depiction.image.horizontal_concat import horizontal_concat
+
 from depiction.image.multi_channel_image import MultiChannelImage
+from depiction.image.multi_channel_image_concatenation import MultiChannelImageConcatenation
 
 app = cyclopts.App()
 
@@ -9,8 +11,8 @@ app = cyclopts.App()
 @app.default
 def concatenate_images(output_hdf5: Path, paths: list[Path]) -> None:
     images = [MultiChannelImage.read_hdf5(path) for path in paths]
-    concatenated = horizontal_concat(images, add_index=True)
-    concatenated.write_hdf5(output_hdf5)
+    concat = MultiChannelImageConcatenation.concat_images(images=images)
+    concat.write_hdf5(path=output_hdf5)
 
 
 if __name__ == "__main__":

14eb29f261e929f62c694263e7fb18a42492f7f9
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-08-06 17:22:46 +0200

    update for new representation of multi image concatenations

--- src/depiction_cluster_sandbox/workflow/proc/compute_image_umap_coefs.py
+++ src/depiction_cluster_sandbox/workflow/proc/compute_image_umap_coefs.py
@@ -4,8 +4,13 @@ import cyclopts
 from umap import UMAP
 
 from depiction.image.multi_channel_image import MultiChannelImage
+from depiction.image.multi_channel_image_concatenation import MultiChannelImageConcatenation
 
 app = cyclopts.App()
+
+# TODO consider how to test individual steps, by checking several assumptions
+#  - extra coordinates preserved
+#  - umap multi-channel image generated at output path
 
 
 @app.default
@@ -15,7 +20,8 @@ def compute_image_umap_coefs(
     n_jobs: int = -1,
     random_state: int | None = None,
 ) -> None:
-    input_image = MultiChannelImage.read_hdf5(path=input_image_path)
+    input_image_conc = MultiChannelImageConcatenation.read_hdf5(path=input_image_path, allow_individual=True)
+    input_image = input_image_conc.get_combined_image()
 
     # compute the umap transformation into 2D
     umap = UMAP(n_components=2, n_jobs=n_jobs, random_state=random_state)
@@ -30,7 +36,8 @@ def compute_image_umap_coefs(
     )
 
     # write it to the output path
-    umap_image.write_hdf5(path=output_image_path)
+    umap_image_conc = input_image_conc.with_replaced_combined_image(image=umap_image)
+    umap_image_conc.write_hdf5(path=output_image_path)
 
 
 if __name__ == "__main__":

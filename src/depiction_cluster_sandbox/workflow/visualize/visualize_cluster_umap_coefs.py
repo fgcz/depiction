d550ec74524a27feed360b6bb3255631834e3ef3
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-08-06 15:25:06 +0200

    make the channel visualized customizable

--- src/depiction_cluster_sandbox/workflow/visualize/visualize_cluster_umap_coefs.py
+++ src/depiction_cluster_sandbox/workflow/visualize/visualize_cluster_umap_coefs.py
@@ -12,20 +12,23 @@ def visualize_cluster_umap_coefs(
     input_umap_hdf5_path: Path,
     input_cluster_hdf5_path: Path,
     output_png_path: Path,
+    channel: str = "cluster",
 ) -> None:
     # load the input data
     umap_image = MultiChannelImage.read_hdf5(path=input_umap_hdf5_path)
     cluster_image = MultiChannelImage.read_hdf5(path=input_cluster_hdf5_path)
 
-    # read the cluster labels
-    cluster_labels = cluster_image.data_flat.sel(c="cluster").values.ravel()
+    # read the labels to visualize
+    labels = cluster_image.data_flat.sel(c=channel).values.ravel()
 
     # read the umap coordinates
     umap_coords = umap_image.data_flat.values.T
 
     # scatter
     fig, ax = plt.subplots(figsize=(10, 10))
-    ax.scatter(umap_coords[:, 0], umap_coords[:, 1], c=cluster_labels, s=1, cmap="tab10")
+    ax.scatter(umap_coords[:, 0], umap_coords[:, 1], c=labels, s=1, cmap="tab10")
+    ax.set_title(f"UMAP of {channel}")
+    ax.axis("off")
 
     # save the figure
     plt.savefig(output_png_path, bbox_inches="tight", pad_inches=0)

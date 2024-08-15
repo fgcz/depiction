4f3468ea50a10dd2822fb5093b27dde4db99c951
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-08-15 09:49:57 +0200

    code to generate a file

--- src/depiction/tools/simulate/generate_synthetic_imzml.py
+++ src/depiction/tools/simulate/generate_synthetic_imzml.py
@@ -2,6 +2,10 @@ import numpy as np
 from numpy.typing import NDArray
 from perlin_noise import PerlinNoise
 
+from depiction.image.multi_channel_image import MultiChannelImage
+from depiction.parallel_ops import WriteSpectraParallel, ParallelConfig
+from depiction.persistence.types import GenericWriteFile
+
 
 class GenerateSyntheticImzml:
     def __init__(self, height: int, width: int, rng: np.random.Generator, mz_min: float, mz_max: float) -> None:
@@ -19,8 +23,7 @@ class GenerateSyntheticImzml:
             [[noise_gen([i / self._width, j / self._height]) for j in range(self._width)] for i in range(self._height)]
         )
         # adjust the values to the desired mean and std
-        noise_2d = (noise_2d - noise_2d.mean()) / noise_2d.std() * std + mean
-        return noise_2d
+        return (noise_2d - noise_2d.mean()) / noise_2d.std() * std + mean
 
     def get_shifted_target_masses(
         self, mass_list: NDArray[float], shift: float, std_noise: float = 0.001
@@ -47,6 +50,27 @@ class GenerateSyntheticImzml:
         int_arr = np.concatenate([target_int * snr, noise_int])
         idx = np.argsort(mz_arr)
         return mz_arr[idx], int_arr[idx]
+
+    def write_file(self, labels: MultiChannelImage, target_masses: NDArray[float], output: GenericWriteFile):
+        # input validation
+        if labels.n_channels != len(target_masses):
+            msg = f"Number of channels in labels ({labels.n_channels}) does not match the number of target masses ({len(target_masses)})"
+            raise ValueError(msg)
+        if labels.dimensions != (self._width, self._height):
+            msg = f"Dimensions of labels ({labels.dimensions}) do not match the dimensions of the synthetic image ({self._width}, {self._height})"
+            raise ValueError(msg)
+
+        # collect the relevant input information
+        shift_map = self.generate_shift_map()
+        label_map = labels.data_spatial.values
+
+        # actually create the file
+        with output.writer() as writer:
+            for i_spectrum, coordinates in enumerate(np.ndindex(self._height, self._width)):
+                shift_value = shift_map[coordinates]
+                target_mz_i = self.get_shifted_target_masses(target_masses, shift=shift_value)
+                mz_arr, int_arr = self.generate_centroided_spectrum(target_mz_i, label_map[coordinates])
+                writer.add_spectrum(mz_arr, int_arr, coordinates)
 
     def _generate_isotopic_peaks(
         self, mz_arr: NDArray[float], int_arr: NDArray[float], n_isotopes: int

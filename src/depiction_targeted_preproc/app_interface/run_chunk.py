5cc52e3367eedc6633b09e66b044fe18164e70c1
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-09-16 10:56:59 +0200

    create initial wrapper for running each job

---
+++ src/depiction_targeted_preproc/app_interface/run_chunk.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+import cyclopts
+import yaml
+
+from depiction_targeted_preproc.pipeline.run_workflow import run_workflow
+
+app = cyclopts.App()
+
+
+@app.default()
+def run_chunk(chunk_dir: Path):
+    zip_file_path = run_workflow(sample_dir=chunk_dir)
+    results = [
+        {
+            "type": "bfabric_copy_resource",
+            "local_path": str(zip_file_path.absolute()),
+            "store_entry_path": zip_file_path.name,
+            # TODO figure out how to abstract this (needs some design changes)
+            "workunit_id": None,
+            "storage_id": None,
+            "update_existing": "if_exists",
+        }
+    ]
+    with (chunk_dir / "outputs.yml").open("w") as f:
+        yaml.safe_dump(results, f)
+
+
+if __name__ == "__main__":
+    app()

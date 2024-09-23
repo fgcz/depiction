c2752606f9288022508c4dd4146a30437f6b5973
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-09-23 08:34:42 +0200

    add some more experimental clustering code

--- src/depiction/tools/clustering.py
+++ src/depiction/tools/clustering.py
@@ -6,9 +6,6 @@ import cyclopts
 import numpy as np
 import xarray
 from depiction.clustering.experimental.amap_clustering import AmapClustering
-from numpy.typing import NDArray
-from sklearn.cluster import KMeans, BisectingKMeans, Birch
-
 from depiction.clustering.extrapolate import extrapolate_labels
 from depiction.clustering.maxmin_sampling import maxmin_sampling
 from depiction.clustering.metrics import cross_correlation
@@ -16,6 +13,8 @@ from depiction.clustering.stratified_grid import StratifiedGrid
 from depiction.image.feature_selection import FeatureSelectionIQR, retain_features
 from depiction.image.image_normalization import ImageNormalization, ImageNormalizationVariant
 from depiction.image.multi_channel_image import MultiChannelImage
+from numpy.typing import NDArray
+from sklearn.cluster import KMeans, BisectingKMeans, Birch
 
 
 class MethodEnum(Enum):
@@ -57,6 +56,49 @@ def get_landmark_indices(
     return np.asarray(indices)
 
 
+def compute_clustering_new_1(
+    input_image: MultiChannelImage,
+    method: MethodEnum,
+    method_params: MethodParamsType,
+    n_best_features: int = 30,
+    n_subsample: int | None = 10000,
+) -> MultiChannelImage:
+    image_features, image_full_features, image_full_image_index = _preprocess(
+        input_image=input_image, n_best_features=n_best_features
+    )
+
+    rng = np.random.default_rng(42)
+    if n_subsample is not None and n_subsample < len(image_features.data_flat):
+        n_subsample = None
+    if n_subsample:
+        grid = StratifiedGrid(cells_x=20, cells_y=20)
+        sampled_features = grid.sample_points(array=image_features.data_flat, n_samples=n_subsample, rng=rng).values.T
+    else:
+        sampled_features = image_features.data_flat.values.T
+
+    # cluster this
+    sampled_labels = compute_labels(features=sampled_features, method=method, method_params=method_params)
+
+    if n_subsample:
+        # extrapolate the labels to the full image
+        full_labels = extrapolate_labels(
+            sampled_features=sampled_features,
+            sampled_labels=sampled_labels,
+            full_features=image_features.data_flat.values.T,
+        )
+    else:
+        full_labels = sampled_labels
+    label_image = MultiChannelImage.from_sparse(
+        values=full_labels[:, np.newaxis],
+        coordinates=image_full_features.coordinates_flat,
+        channel_names=["cluster"],
+        bg_value=np.nan,
+    )
+
+    # return the result of the operation
+    return MultiChannelImage(xarray.concat([label_image.data_spatial, input_image.data_spatial], dim="c"))
+
+
 def compute_clustering(
     input_image: MultiChannelImage,
     method: MethodEnum,
@@ -68,23 +110,8 @@ def compute_clustering(
 ) -> MultiChannelImage:
     rng = np.random.default_rng(42)
 
-    # massage the input image
-    assert "cluster" not in input_image.channel_names
-    image_full_features = input_image.drop_channels(coords=["image_index"], allow_missing=True)
-    image_full_features = ImageNormalization().normalize_image(
-        image=image_full_features, variant=ImageNormalizationVariant.STD
-    )
-    if "image_index" in input_image.channel_names:
-        image_full_image_index = input_image.retain_channels(coords=["image_index"])
-    else:
-        with xarray.set_options(keep_attrs=True):
-            image_full_image_index = MultiChannelImage(
-                xarray.zeros_like(input_image.data_spatial.isel(c=[0])).assign_coords(c=["image_index"])
-            )
-
-    # retain the most relevant features
-    image_features = retain_features(
-        feature_selection=FeatureSelectionIQR.model_validate({"n_features": n_best_features}), image=image_full_features
+    image_features, image_full_features, image_full_image_index = _preprocess(
+        input_image=input_image, n_best_features=n_best_features
     )
 
     # sample a number of landmark features which will be used for correlation-based clustering
@@ -125,6 +152,27 @@ def compute_clustering(
 
     # return the result of the operation
     return MultiChannelImage(xarray.concat([label_image.data_spatial, input_image.data_spatial], dim="c"))
+
+
+def _preprocess(input_image, n_best_features):
+    # massage the input image
+    assert "cluster" not in input_image.channel_names
+    image_full_features = input_image.drop_channels(coords=["image_index"], allow_missing=True)
+    image_full_features = ImageNormalization().normalize_image(
+        image=image_full_features, variant=ImageNormalizationVariant.STD
+    )
+    if "image_index" in input_image.channel_names:
+        image_full_image_index = input_image.retain_channels(coords=["image_index"])
+    else:
+        with xarray.set_options(keep_attrs=True):
+            image_full_image_index = MultiChannelImage(
+                xarray.zeros_like(input_image.data_spatial.isel(c=[0])).assign_coords(c=["image_index"])
+            )
+    # retain the most relevant features
+    image_features = retain_features(
+        feature_selection=FeatureSelectionIQR.model_validate({"n_features": n_best_features}), image=image_full_features
+    )
+    return image_features, image_full_features, image_full_image_index
 
 
 @app.default()

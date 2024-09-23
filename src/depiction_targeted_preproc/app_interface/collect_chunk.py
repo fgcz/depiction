b33d0e182d24eed80fb8d56d12d8af6f2c7d7787
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-09-23 13:42:40 +0200

    make the two implementations analogous to each other

--- src/depiction_targeted_preproc/app_interface/collect_chunk.py
+++ src/depiction_targeted_preproc/app_interface/collect_chunk.py
@@ -3,6 +3,9 @@ from pathlib import Path
 import cyclopts
 import yaml
 
+from bfabric import Bfabric
+from bfabric.experimental.app_interface.workunit.definition import WorkunitDefinition
+
 app = cyclopts.App()
 
 
@@ -11,11 +14,14 @@ def collect_chunk(workunit_ref: int | Path, chunk_dir: Path) -> None:
     chunk_dir = chunk_dir.absolute()
     zip_file_path = chunk_dir / f"{chunk_dir.name}.zip"
 
+    workunit_definition = WorkunitDefinition.from_ref(workunit_ref, client=Bfabric.from_config())
+    workunit_id = workunit_definition.registration.workunit_id
+
     outputs = [
         {
             "type": "bfabric_copy_resource",
             "local_path": str(zip_file_path.absolute()),
-            "store_entry_path": zip_file_path.name,
+            "store_entry_path": f"WU{workunit_id}_result_{chunk_dir.name}.zip",
         }
     ]
     result = {"outputs": outputs}

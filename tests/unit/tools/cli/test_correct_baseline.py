a9629c29a16521142eccb30463f7d8e5861418d9
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-10-11 11:24:05 +0200

    update test to reflect the removal of the old "zero" variant

--- tests/unit/tools/cli/test_correct_baseline.py
+++ tests/unit/tools/cli/test_correct_baseline.py
@@ -3,26 +3,11 @@ from pathlib import Path
 import pytest
 from pytest_mock import MockerFixture
 
-from depiction.persistence import ImzmlReadFile
 from depiction.tools.cli.cli_correct_baseline import run
 from depiction.tools.correct_baseline import BaselineVariants
 
 
-def test_run_when_variant_zero(mocker: MockerFixture) -> None:
-    mock_copy_to = mocker.patch.object(ImzmlReadFile, "copy_to")
-    mock_logger = mocker.patch("depiction.tools.correct_baseline.logger")
-    mock_input_imzml = Path("/dev/null/hello.imzML")
-    mock_output_imzml = mocker.MagicMock(name="mock_output_imzml")
-
-    run(input_imzml=mock_input_imzml, output_imzml=mock_output_imzml, baseline_variant=BaselineVariants.Zero)
-
-    mock_copy_to.assert_called_once_with(mock_output_imzml)
-    mock_output_imzml.parent.mkdir.assert_called_once_with(parents=True, exist_ok=True)
-    mock_logger.info.assert_called_once()
-
-
 def test_run_when_other_variant(mocker: MockerFixture) -> None:
-    mock_logger = mocker.patch("loguru.logger")
     mock_imzml_mode = mocker.MagicMock(name="mock_imzml_mode", spec=[])
     construct_imzml_read_file = mocker.patch("depiction.tools.correct_baseline.ImzmlReadFile")
     construct_imzml_read_file.return_value.imzml_mode = mock_imzml_mode

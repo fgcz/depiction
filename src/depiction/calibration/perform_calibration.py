0b5b9f6d377fc95a0028c7e58ad15750f5609d32
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-10-14 14:36:36 +0200

    experimental changes

--- src/depiction/calibration/perform_calibration.py
+++ src/depiction/calibration/perform_calibration.py
@@ -10,6 +10,7 @@ from numpy.typing import NDArray
 from xarray import DataArray
 
 from depiction.calibration.calibration_method import CalibrationMethod
+from depiction.image import MultiChannelImage
 from depiction.parallel_ops import ParallelConfig, ReadSpectraParallel, WriteSpectraParallel
 from depiction.parallel_ops.parallel_map import ParallelMap
 from depiction.persistence.types import GenericReadFile, GenericWriteFile, GenericReader, GenericWriter
@@ -25,22 +26,6 @@ class PerformCalibration:
         self._calibration = calibration
         self._parallel_config = parallel_config
         self._coefficient_output_file = coefficient_output_file
-
-    # def _reshape(self, pattern: str, data: DataArray, coordinates) -> DataArray:
-    #    if pattern == "i,c->y,x,c":
-    #        data = data.copy()
-    #        # TODO fix the deprecation here!
-    #        data["i"] = pd.MultiIndex.from_arrays((coordinates[:, 1], coordinates[:, 0]), names=("y", "x"))
-    #        data = data.unstack("i")
-    #        return data.transpose("y", "x", "c")
-    #    elif pattern == "y,x,c->i,c":
-    #        data = data.transpose("y", "x", "c").copy()
-    #        data = data.stack(i=("y", "x")).drop_vars(["i", "x", "y"])
-    #        # convert to integers
-    #        data["i"] = np.arange(len(data["i"]))
-    #        return data.transpose("i", "c")
-    #    else:
-    #        raise ValueError(f"Unknown pattern={repr(pattern)}")
 
     def _validate_per_spectra_array(self, array: DataArray, coordinates_2d: NDArray[float]) -> None:
         """Checks the DataArray has the correct shapes and dimensions. Used for debugging."""
@@ -75,28 +60,24 @@ class PerformCalibration:
     def calibrate_image(
         self, read_peaks: GenericReadFile, write_file: GenericWriteFile, read_full: Optional[GenericReadFile] = None
     ) -> None:
-        if read_full is None:
-            read_full = read_peaks
+        read_full = read_full or read_peaks
 
         logger.info("Extracting all features...")
-        all_features = self._extract_all_features(read_peaks).transpose("i", "c")
-        self._validate_per_spectra_array(all_features, coordinates_2d=read_peaks.coordinates_2d)
+        all_features = self._extract_all_features(read_peaks)
         self._write_data_array(all_features, group="features_raw")
 
         logger.info("Preprocessing features...")
-        all_features = self._calibration.preprocess_image_features(all_features=all_features).transpose("i", "c")
-        self._validate_per_spectra_array(all_features, coordinates_2d=read_peaks.coordinates_2d)
+        all_features = self._calibration.preprocess_image_features(all_features=all_features)
         self._write_data_array(all_features, group="features_processed")
 
         logger.info("Fitting models...")
-        model_coefs = self._fit_all_models(all_features=all_features).transpose("i", "c")
-        self._validate_per_spectra_array(model_coefs, coordinates_2d=read_peaks.coordinates_2d)
+        model_coefs = self._fit_all_models(all_features=all_features)
         self._write_data_array(model_coefs, group="model_coefs")
 
         logger.info("Applying models...")
         self._apply_all_models(read_file=read_full, write_file=write_file, all_model_coefs=model_coefs)
 
-    def _extract_all_features(self, read_peaks: GenericReadFile) -> DataArray:
+    def _extract_all_features(self, read_peaks: GenericReadFile) -> MultiChannelImage:
         read_parallel = ReadSpectraParallel.from_config(self._parallel_config)
         all_features = read_parallel.map_chunked(
             read_file=read_peaks,
@@ -106,12 +87,14 @@ class PerformCalibration:
             ),
             reduce_fn=lambda chunks: xarray.concat(chunks, dim="i"),
         )
-        return all_features.assign_coords(
-            x=("i", read_peaks.coordinates_2d[:, 0]), y=("i", read_peaks.coordinates_2d[:, 1])
+        return MultiChannelImage.from_flat(
+            values=all_features,
+            coordinates=DataArray(read_peaks.coordinates_2d, dims=["i", "d"], coords={"d": ["x", "y"]}),
+            channel_names=None,
         )
 
     def _apply_all_models(
-        self, read_file: GenericReadFile, write_file: GenericWriteFile, all_model_coefs: DataArray
+        self, read_file: GenericReadFile, write_file: GenericWriteFile, all_model_coefs: MultiChannelImage
     ) -> None:
         write_parallel = WriteSpectraParallel.from_config(self._parallel_config)
         write_parallel.map_chunked_to_file(
@@ -124,15 +107,17 @@ class PerformCalibration:
             ),
         )
 
-    def _fit_all_models(self, all_features: DataArray) -> DataArray:
+    def _fit_all_models(self, all_features: MultiChannelImage) -> MultiChannelImage:
         parallel_map = ParallelMap.from_config(self._parallel_config)
+        # TODO to be refactored
+        all_features_flat = all_features.data_flat
         result = parallel_map(
             operation=self._fit_chunk_models,
-            tasks=np.array_split(all_features.coords["i"], self._parallel_config.n_jobs),
+            tasks=np.array_split(all_features_flat.coords["i"], self._parallel_config.n_jobs),
             reduce_fn=lambda chunks: xarray.concat(chunks, dim="i"),
-            bind_kwargs={"all_features": all_features},
+            bind_kwargs={"all_features": all_features_flat},
         )
-        return result
+        return MultiChannelImage.from_flat(result, coordinates=all_features.coordinates_flat, channel_names=None)
 
     def _fit_chunk_models(self, spectra_indices: NDArray[int], all_features: DataArray) -> DataArray:
         collect = []
@@ -140,13 +125,14 @@ class PerformCalibration:
             features = all_features.sel(i=spectrum_id)
             model_coef = self._calibration.fit_spectrum_model(features=features)
             collect.append(model_coef)
-        combined = xarray.concat(collect, dim="i")
-        combined.coords["i"] = spectra_indices
-        return combined
+        return xarray.concat(collect, dim="i")
 
-    def _write_data_array(self, array: DataArray, group: str) -> None:
+    def _write_data_array(self, image: MultiChannelImage, group: str) -> None:
         if not self._coefficient_output_file:
             return
+
+        # TODO this uses deprecated functionality but needs to be handled somewhere more generally later
+        array = image.data_flat.assign_coords({"i": np.arange(len(image.data_flat.i))})
         # TODO engine should not be necessary, but using it for debugging
         array.to_netcdf(path=self._coefficient_output_file, group=group, format="NETCDF4", engine="netcdf4", mode="a")
 
@@ -171,12 +157,12 @@ class PerformCalibration:
         spectra_indices: list[int],
         writer: GenericWriter,
         calibration: CalibrationMethod,
-        all_model_coefs: DataArray,
+        all_model_coefs: MultiChannelImage,
     ) -> None:
         for spectrum_id in spectra_indices:
             # TODO sanity check the usage of i as spectrum_id (i.e. check the coords!)
             mz_arr, int_arr, coords = reader.get_spectrum_with_coords(spectrum_id)
-            features = all_model_coefs.sel(i=spectrum_id)
+            features = all_model_coefs.data_flat.isel(i=spectrum_id)
             calib_mz_arr, calib_int_arr = calibration.apply_spectrum_model(
                 spectrum_mz_arr=mz_arr, spectrum_int_arr=int_arr, model_coef=features
             )

6726da656bf051ee78548a62b61cdb23b9a864d2
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-10-14 16:48:11 +0200

    extract the alpha channel functionality

--- src/depiction/image/multi_channel_image_persistence.py
+++ src/depiction/image/multi_channel_image_persistence.py
@@ -5,8 +5,11 @@ from typing import TYPE_CHECKING, Literal
 
 import xarray
 
+from depiction.image.container.alpha_stack import AlphaStack
+
 if TYPE_CHECKING:
     from depiction.image.multi_channel_image import MultiChannelImage
+
 
 # TODO currently the files are not closed, which you can observe e.g. in a notebook when the original files has been
 #      replaced
@@ -17,20 +20,18 @@ class MultiChannelImagePersistence:
 
     def __init__(self, image: MultiChannelImage) -> None:
         self._image = image
+        self._alpha_channel = AlphaStack(label=image.is_foreground_label)
 
     def write_hdf5(self, path: Path, mode: Literal["a", "w"] = "w", group: str | None = None) -> None:
         data_array = self._image.data_spatial
         is_fg_array = self._image.fg_mask
-        is_fg_label = self._image.is_foreground_label
 
         if not isinstance(data_array.coords["c"][0].item(), str):
             # TODO this really should be validated against in the constructor, and the static methods need to set it
             #   TODO FIXME later
             data_array = data_array.assign_coords(c=self._image.channel_names)
 
-        combined_array = self._stack_for_persistence(
-            data_array=data_array, is_fg_array=is_fg_array, is_fg_label=is_fg_label
-        )
+        combined_array = self._alpha_channel.stack(data_array=data_array, is_fg_array=is_fg_array)
         # TODO engine should not be necessary, but using it for debugging
         combined_array.to_netcdf(path, mode=mode, group=group, format="NETCDF4", engine="netcdf4")
 
@@ -41,23 +42,7 @@ class MultiChannelImagePersistence:
         from depiction.image.multi_channel_image import MultiChannelImage
 
         combined_array = xarray.open_dataarray(path, group=group)
-        data_array, is_fg_array = cls._split_from_combined(combined=combined_array, is_fg_label=is_foreground_label)
+        data_array, is_fg_array = AlphaStack(label=is_foreground_label).split(combined=combined_array)
         return MultiChannelImage(data=data_array, is_foreground=is_fg_array, is_foreground_label=is_foreground_label)
-
-    @classmethod
-    def _stack_for_persistence(
-        cls, data_array: xarray.DataArray, is_fg_array: xarray.DataArray, is_fg_label: str
-    ) -> xarray.DataArray:
-        return xarray.concat(
-            [data_array, is_fg_array.expand_dims("c", axis=-1).assign_coords(c=[is_fg_label])], dim="c"
-        )
-
-    @classmethod
-    def _split_from_combined(
-        cls, combined: xarray.DataArray, is_fg_label: str
-    ) -> tuple[xarray.DataArray, xarray.DataArray]:
-        data_array = combined.drop_sel(c=is_fg_label)
-        is_fg_array = combined.sel(c=is_fg_label).drop_vars("c").astype(bool)
-        return data_array, is_fg_array
 
     # TODO is_valid_hdf5

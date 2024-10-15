143678bcd6a97bfc4e099570421717eb0cc2e67d
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-10-15 16:25:32 +0200

    impl min_filter by absolute

--- src/depiction/image/smoothing/min_filter.py
+++ src/depiction/image/smoothing/min_filter.py
@@ -1,18 +1,25 @@
 from dataclasses import dataclass
+from enum import Enum
 
-import scipy.ndimage
+import numpy as np
 from xarray import DataArray
 
 from depiction.image import MultiChannelImage
 from depiction.image.xarray_helper import XarrayHelper
 
 
+# TODO this is a prototype, not optimized and not tested!
 # TODO if useful, this can be made a lot more generic
 
 
-class KernelShape:
+class KernelShape(Enum):
     Square = "square"
     Circle = "circle"
+
+
+class KernelFunction(Enum):
+    Min = "min"
+    AbsMin = "abs_min"
 
 
 @dataclass(frozen=True)
@@ -24,11 +31,27 @@ class MinFilter:
         data = XarrayHelper.ensure_dense(image.data_spatial)
         if self.kernel_shape != KernelShape.Square:
             raise ValueError("Only square kernel is supported for now")
-        # TODO i want to try the circular kernel later, this impl is just for quick prototyping (as it does not support
-        #      a circular window)
-        smoothed = scipy.ndimage.minimum_filter(data, size=self.kernel_size, axes=(0, 1))
+
+        smoothed_data = np.zeros_like(data.values)
+        for c in range(smoothed_data.shape[2]):
+            smoothed_data[:, :, c] = _eval_abs_min(data.values[:, :, c], self.kernel_size)
+
         return MultiChannelImage(
-            DataArray(smoothed, dims=data.dims, coords=data.coords),
+            DataArray(smoothed_data, dims=data.dims, coords=data.coords),
             is_foreground=image.fg_mask,
             is_foreground_label=image.is_foreground_label,
         )
+
+
+def _min_by_abs(arr):
+    assert arr.ndim == 1
+    return arr[np.abs(arr).argmin()]
+
+
+def _eval_abs_min(array, ws: int):
+    result = np.zeros_like(array)
+    for i in range(array.shape[0]):
+        for j in range(array.shape[1]):
+            window = array[max(i - ws, 0) : i + ws + 1, max(j - ws, 0) : j + ws + 1]
+            result[i, j] = _min_by_abs(window.ravel())
+    return result

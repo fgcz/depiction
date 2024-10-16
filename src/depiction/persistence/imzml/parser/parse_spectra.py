9811c45a5b74288a7991f9c3ad7428ddf9f0d57b
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-10-16 14:11:49 +0200

    parse the spectra entries

--- src/depiction/persistence/imzml/parser/parse_spectra.py
+++ src/depiction/persistence/imzml/parser/parse_spectra.py
@@ -9,6 +9,26 @@ class _CvParam:
     value: str
 
 
+@dataclass
+class _BinaryArray:
+    array_length: int
+    encoded_length: int
+    offset: int
+    param_groups: list[str]
+
+
+@dataclass
+class _SpectrumStatic:
+    id: str
+    index: int
+    param_groups: list[str]
+    param_groups_scan: list[str]
+    params: list[_CvParam]
+    params_scan: list[_CvParam]
+    position: tuple[int, int] | tuple[int, int, int]
+    binary_arrays: list[_BinaryArray]
+
+
 class ParseSpectra:
     def __init__(self, etree: ElementTree):
         self._etree = etree
@@ -19,12 +39,75 @@ class ParseSpectra:
         elements = self._etree.findall(f"/{self._ns}referenceableParamGroupList/{self._ns}referenceableParamGroup")
         result = {}
         for element in elements:
-            result[element.attrib["id"]] = [
-                _CvParam(
-                    accession=cv_param.attrib["accession"],
-                    name=cv_param.attrib["name"],
-                    value=cv_param.attrib.get("value", ""),
-                )
-                for cv_param in element.findall(f"{self._ns}cvParam")
-            ]
+            result[element.attrib["id"]] = self._extract_cv_param_element_list(element.findall(f"{self._ns}cvParam"))
         return result
+
+    @property
+    def _spectra_static(self) -> list[_SpectrumStatic]:
+        # TODO multi run support...
+        elements = self._etree.findall(f"/{self._ns}run/{self._ns}spectrumList/{self._ns}spectrum")
+        results = []
+        for element in elements:
+            param_groups = [ref.attrib["ref"] for ref in element.findall(f"{self._ns}referenceableParamGroupRef")]
+            param_groups_scan = [
+                ref.attrib["ref"]
+                for ref in element.findall(f"{self._ns}scanList/{self._ns}scan/{self._ns}referenceableParamGroupRef")
+            ]
+
+            position_x = element.find(f"{self._ns}scanList/{self._ns}scan/{self._ns}cvParam[@accession='IMS:1000050']")
+            position_y = element.find(f"{self._ns}scanList/{self._ns}scan/{self._ns}cvParam[@accession='IMS:1000051']")
+            position_z = element.find(f"{self._ns}scanList/{self._ns}scan/{self._ns}cvParam[@accession='IMS:1000052']")
+
+            if not position_z:
+                position = (int(position_x.attrib["value"]), int(position_y.attrib["value"]))
+            else:
+                position = (
+                    int(position_x.attrib["value"]),
+                    int(position_y.attrib["value"]),
+                    int(position_z.attrib["value"]),
+                )
+
+            binary_arrays = []
+            for binary_element in element.findall(f"{self._ns}binaryDataArrayList/{self._ns}binaryDataArray"):
+                binary_arrays.append(
+                    _BinaryArray(
+                        array_length=int(
+                            binary_element.find(f"{self._ns}cvParam[@accession='IMS:1000103']").attrib["value"]
+                        ),
+                        encoded_length=int(
+                            binary_element.find(f"{self._ns}cvParam[@accession='IMS:1000104']").attrib["value"]
+                        ),
+                        offset=int(binary_element.find(f"{self._ns}cvParam[@accession='IMS:1000102']").attrib["value"]),
+                        param_groups=[
+                            ref.attrib["ref"] for ref in binary_element.findall(f"{self._ns}referenceableParamGroupRef")
+                        ],
+                    )
+                )
+
+            results.append(
+                _SpectrumStatic(
+                    id=element.attrib["id"],
+                    index=int(element.attrib["index"]),
+                    param_groups=param_groups,
+                    param_groups_scan=param_groups_scan,
+                    params=self._extract_cv_param_element_list(element.findall(f"{self._ns}cvParam")),
+                    params_scan=self._extract_cv_param_element_list(
+                        element.findall(f"{self._ns}scanList/{self._ns}scan/{self._ns}cvParam")
+                    ),
+                    position=position,
+                    binary_arrays=binary_arrays,
+                )
+            )
+
+        return results
+
+    @staticmethod
+    def _extract_cv_param_element_list(els):
+        return [
+            _CvParam(
+                accession=el.attrib["accession"],
+                name=el.attrib["name"],
+                value=el.attrib.get("value", ""),
+            )
+            for el in els
+        ]
--- tests/integration/imzml_parser/test_parse_spectra.py
+++ tests/integration/imzml_parser/test_parse_spectra.py
@@ -3,6 +3,7 @@ from xml.etree import ElementTree
 
 import pytest
 
+from depiction.persistence.imzml.parser.parse_spectra import ParseSpectra
 from depiction.persistence.imzml.parser.parse_spectra import _CvParam
 
 
@@ -12,9 +13,13 @@ def etree_referenceable_param_groups():
     return ElementTree.parse(path)
 
 
-def test_referenceable_param_groups(etree_referenceable_param_groups):
-    from depiction.persistence.imzml.parser.parse_spectra import ParseSpectra
+@pytest.fixture
+def etree_spectra_static():
+    path = Path(__file__).parent / "chunks" / "spectra_static.xml"
+    return ElementTree.parse(path)
 
+
+def test_referenceable_param_groups(etree_referenceable_param_groups):
     parser = ParseSpectra(etree_referenceable_param_groups)
     referenceable_param_groups = parser._referenceable_param_groups
 
@@ -31,3 +36,33 @@ def test_referenceable_param_groups(etree_referenceable_param_groups):
         _CvParam(accession="MS:1000574", name="zlib compression", value=""),
         _CvParam(accession="IMS:1000101", name="external data", value="true"),
     ]
+
+
+def test_spectra_static(etree_spectra_static):
+    parser = ParseSpectra(etree_spectra_static)
+    spectra_static = parser._spectra_static
+    assert len(spectra_static) == 3
+    assert spectra_static[0].id == "spectrum=1"
+    assert spectra_static[0].index == 1
+    assert spectra_static[0].param_groups == ["spectrum1"]
+    assert spectra_static[0].param_groups_scan == ["scan1"]
+    assert spectra_static[0].params == [
+        _CvParam(accession="MS:1000528", name="lowest observed m/z", value="803.150729499946"),
+        _CvParam(accession="MS:1000527", name="highest observed m/z", value="1721.4524145370692"),
+        _CvParam(accession="MS:1000504", name="base peak m/z", value="1482.7548111416522"),
+        _CvParam(accession="MS:1000505", name="base peak intensity", value="3667.0"),
+        _CvParam(accession="MS:1000285", name="total ion current", value="23902.0"),
+    ]
+    assert spectra_static[0].params_scan == [
+        _CvParam(accession="IMS:1000050", name="position x", value="2445"),
+        _CvParam(accession="IMS:1000051", name="position y", value="822"),
+    ]
+    assert spectra_static[0].position == (2445, 822)
+    assert len(spectra_static[0].binary_arrays) == 2
+
+    assert spectra_static[0].binary_arrays[0].array_length == 82
+    assert spectra_static[0].binary_arrays[0].encoded_length == 667
+    assert spectra_static[0].binary_arrays[0].offset == 16
+    assert spectra_static[0].binary_arrays[0].param_groups == ["mzArray"]
+
+    assert spectra_static[0].binary_arrays[1].param_groups == ["intensityArray"]

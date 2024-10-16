ddc13997dd1dbdef3b7b400a1fe882ed5907882b
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-10-16 15:27:37 +0200

    implement percentile based filtering

--- src/depiction/image/smoothing/min_filter.py
+++ src/depiction/image/smoothing/min_filter.py
@@ -11,6 +11,15 @@ from depiction.image.xarray_helper import XarrayHelper
 # TODO this is a prototype, not optimized and not tested!
 # TODO if useful, this can be made a lot more generic
 
+# Some consideration for the intuitions
+# - Consider each channel separately, as we have no trust in the correspondences and this will be handled by the
+#   regression. (+)
+# - If we have a constant value in the window, we can just take that value. (+)
+# - If we the center is higher than the surrounding,
+#   - we can take a lower value if we are sure that this high intensity is essentially a noisy artifact
+#   - we should not take a higher value
+#   - but to make this robust we might want to consider using medians...
+
 
 class KernelShape(Enum):
     Square = "square"
@@ -26,6 +35,7 @@ class KernelFunction(Enum):
 class MinFilter:
     kernel_size: int = 5
     kernel_shape: KernelShape = KernelShape.Square
+    percentile: float = 0
 
     def smooth_image(self, image: MultiChannelImage) -> MultiChannelImage:
         data = XarrayHelper.ensure_dense(image.data_spatial)
@@ -34,7 +44,10 @@ class MinFilter:
 
         smoothed_data = np.zeros_like(data.values)
         for c in range(smoothed_data.shape[2]):
-            smoothed_data[:, :, c] = _eval_abs_min(data.values[:, :, c], self.kernel_size)
+            if self.percentile == 0:
+                smoothed_data[:, :, c] = _eval_abs_min(data.values[:, :, c], self.kernel_size)
+            else:
+                smoothed_data[:, :, c] = _eval_abs_percentile(data.values[:, :, c], self.kernel_size, self.percentile)
 
         return MultiChannelImage(
             DataArray(smoothed_data, dims=data.dims, coords=data.coords),
@@ -54,4 +67,13 @@ def _eval_abs_min(array, ws: int):
         for j in range(array.shape[1]):
             window = array[max(i - ws, 0) : i + ws + 1, max(j - ws, 0) : j + ws + 1]
             result[i, j] = _min_by_abs(window.ravel())
+    return result
+
+
+def _eval_abs_percentile(array, ws: int, percentile: float):
+    result = np.zeros_like(array)
+    for i in range(array.shape[0]):
+        for j in range(array.shape[1]):
+            window = array[max(i - ws, 0) : i + ws + 1, max(j - ws, 0) : j + ws + 1].ravel()
+            result[i, j] = window[np.argsort(np.abs(window))[int(percentile * len(window))]]
     return result

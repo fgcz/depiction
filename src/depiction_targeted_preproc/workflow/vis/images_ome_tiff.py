424eb8b4455deaca68cf7d35126cce92ce04b2cf
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-10-16 15:37:11 +0200

    don't export alpha channel to ome-tiff

--- src/depiction_targeted_preproc/workflow/vis/images_ome_tiff.py
+++ src/depiction_targeted_preproc/workflow/vis/images_ome_tiff.py
@@ -1,33 +1,32 @@
 from pathlib import Path
-from typing import Annotated
 
-import typer
-import xarray
+import cyclopts
 
-from depiction.persistence.imzml.extract_metadata import Metadata
+from depiction.image import MultiChannelImage
 from depiction.persistence.format_ome_tiff import OmeTiff
+from depiction.persistence.imzml.extract_metadata import Metadata
+
+app = cyclopts.App()
 
 
+@app.default
 def vis_images_ome_tiff(
-    input_netcdf_path: Annotated[Path, typer.Option()],
-    input_raw_metadata_path: Annotated[Path, typer.Option()],
-    output_ometiff_path: Annotated[Path, typer.Option()],
+    input_netcdf_path: Path,
+    input_raw_metadata_path: Path,
+    output_ometiff_path: Path,
 ) -> None:
     # load the netcdf data
-    image = xarray.open_dataset(input_netcdf_path).to_array("var").squeeze("var")
+    image = MultiChannelImage.read_hdf5(input_netcdf_path)
+    data = image.data_spatial
 
     # add image resolution
     raw_metadata = Metadata.model_validate_json(input_raw_metadata_path.read_text())
     # TODO test the implication on save/load
-    image.attrs["pixel_size"] = raw_metadata.pixel_size
+    data.attrs["pixel_size"] = raw_metadata.pixel_size
 
     # perform the export
-    OmeTiff.write(image=image, path=output_ometiff_path)
-
-
-def main() -> None:
-    typer.run(vis_images_ome_tiff)
+    OmeTiff.write(image=data, path=output_ometiff_path)
 
 
 if __name__ == "__main__":
-    main()
+    app()

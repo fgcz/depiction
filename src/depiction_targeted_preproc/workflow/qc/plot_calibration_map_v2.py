91af5b8c8aea2f3f2501718d93257fe7ddde290a
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-10-16 11:28:45 +0200

    add more info to plot

--- src/depiction_targeted_preproc/workflow/qc/plot_calibration_map_v2.py
+++ src/depiction_targeted_preproc/workflow/qc/plot_calibration_map_v2.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 from typing import Annotated
 
+import numpy as np
 import typer
 import xarray
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt, colors
 from typer import Option
 
 
@@ -13,18 +14,36 @@ def qc_plot_calibration_map_v2(
 ) -> None:
     mass_shifts = xarray.open_dataarray(input_mass_shifts)
 
-    fig, axs = plt.subplots(2, 1, figsize=(10, 20))
+    fig, axs = plt.subplots(3, 1, figsize=(10, 20))
 
     # show the map
-    mass_shifts.isel(c=0).plot.imshow(x="x", y="y", ax=axs[0], cmap="coolwarm", vmin=-0.5, vmax=+0.5, yincrease=False)
+    vmax = np.percentile(np.abs(mass_shifts.isel(c=0).values.ravel()), 0.99)
+    mass_shifts.isel(c=0).plot.imshow(x="x", y="y", ax=axs[0], cmap="RdBu_r", vmin=-vmax, vmax=+vmax, yincrease=False)
     axs[0].set_aspect("equal")
     test_mass = mass_shifts.coords["c"][0]
-    axs[0].set_title(f"Computed shift for test mass {test_mass:.2f}")
+    axs[0].set_title(f"Computed shift for test mass {test_mass:.2f} (linear)")
+
+    # show a more qualitative map
+    mass_shifts.isel(c=0).plot.imshow(
+        x="x",
+        y="y",
+        ax=axs[1],
+        cmap="RdBu_r",
+        yincrease=False,
+        norm=colors.SymLogNorm(linthresh=0.001, vmin=-1, vmax=1),
+    )
+    # contour
+    mass_shifts.isel(c=0).plot.contour(x="x", y="y", ax=axs[1], colors="black", yincrease=False, alpha=0.3)
+    axs[1].set_aspect("equal")
+    axs[1].set_title(f"Computed shift for test mass {test_mass:.2f} (symlog)")
 
     # show the histogram
     # TODO the clipping could be misleading (as it's not indicated)
-    mass_shifts.isel(c=0).clip(-0.5, 0.5).plot.hist(ax=axs[1], bins=100, color="gray")
-    axs[1].set_title("Histogram of computed shifts")
+    num_nans = np.sum(np.isnan(mass_shifts.isel(c=0)).values)
+    if num_nans:
+        raise ValueError("nans detected")
+    mass_shifts.isel(c=0).clip(-0.5, 0.5).plot.hist(ax=axs[2], bins=100, color="gray")
+    axs[2].set_title("Histogram of computed shifts")
 
     plt.savefig(output_pdf, bbox_inches="tight")
 

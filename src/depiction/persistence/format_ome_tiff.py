2c1d973af6f2daaae83354f3e3ccd711231778fb
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-10-22 17:05:04 +0200

    almost working version

--- src/depiction/persistence/format_ome_tiff.py
+++ src/depiction/persistence/format_ome_tiff.py
@@ -1,13 +1,11 @@
 # TODO figure out if format specific exporters should actually be moved to a different path
 # TODO figure out the ideal extension i.e. tif vs tiff!
-import contextlib
 from pathlib import Path
 from typing import Any
 
-import numpy as np
+import ome_types
 import tifffile
 import xarray
-import ome_types
 
 from depiction.persistence.pixel_size import PixelSize
 
@@ -23,26 +21,56 @@ class OmeTiff:
         """Writes the image to a OME-TIFF file at the specified path.
         The image must have the dimensions c, y, x and an attribute "pixel_size" with the pixel size information.
         """
-        pixel_size = image.attrs["pixel_size"]
         channel_names = list(image.coords["c"].values)
-
-        metadata = {
-            **cls._get_image_resolution_metadata(pixel_size),
-            **cls._get_channel_metadata(channel_names),
-        }
-
-        # add dimensions for the OME-TIFF format
-        image_export = image.expand_dims(dim=["z", "t"])
+        image_export = image.expand_dims("s").transpose("c", "y", "x", "s")
 
         # export
-        writer = pyometiff.OMETIFFWriter(
-            fpath=path,
-            dimension_order="ZTCYX",
-            array=image_export.transpose("z", "t", "c", "y", "x").values,
-            metadata=metadata,
-            explicit_tiffdata=True,
+        tifffile.imwrite(
+            path,
+            image_export.data,
+            metadata={"axes": "CYXS", "channel_names": channel_names},
         )
-        writer.write()
+
+        """
+        pixel_size = image.attrs["pixel_size"]
+        #metadata = {
+        #    **cls._get_image_resolution_metadata(pixel_size),
+        #    **cls._get_channel_metadata(channel_names),
+        #}
+
+        # add storage dimensions for the OME-TIFF format
+        #image_export = image.expand_dims("s").transpose("c", "y", "x", "s")
+        image_export = image.expand_dims(["z", "t"]).transpose( "x", "y", "z", "c", "t")
+        #image_export = image.expand_dims("s").transpose("c", "y", "x", "c", "t")
+
+        # create OME-XML metadata
+        pixels_data = ome_types.model.Pixels(
+            # TODO check "PixelType"
+            type="float",
+            size_c=image_export.sizes["c"],
+            size_t=1,
+            size_z=1,
+            size_x=image_export.sizes["x"],
+            size_y=image_export.sizes["y"],
+            dimension_order="XYZCT",
+            channels=[{"name": name} for name in channel_names],
+            physical_size_x=pixel_size.size_x,
+            physical_size_y=pixel_size.size_y,
+        )
+        image_data = ome_types.model.Image(pixels=pixels_data)
+        ome_data = ome_types.model.OME(images=[image_data])
+        ome_xml = ome_types.to_xml(ome_data)
+
+        # export
+        tifffile.imwrite(
+            path,
+            image_export.data.T,
+            description=ome_xml,
+            metadata=None,
+            ome=True,
+            photometric="minisblack",
+            #metadata={"axes": "CYXS", **cls._get_channel_metadata(channel_names)},
+        )"""
 
     @classmethod
     def read(cls, path: Path) -> xarray.DataArray:
@@ -57,9 +85,10 @@ class OmeTiff:
             raise ValueError("Only single image OME-TIFF files are supported")
 
         image_data = ome_data.images[0]
+        np_array = file.asarray(squeeze=False).T[0]
         array = (
             xarray.DataArray(
-                file.asarray(squeeze=False).T[0],
+                np_array,
                 dims=[char.lower() for char in image_data.pixels.dimension_order.value],
                 coords={"c": [channel.name for channel in image_data.pixels.channels]},
             )

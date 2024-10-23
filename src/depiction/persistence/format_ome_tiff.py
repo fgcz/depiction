14ad61b582dda51946c4d06fdf2362c2033463eb
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-10-23 10:25:50 +0200

    use bioio - it just works

--- src/depiction/persistence/format_ome_tiff.py
+++ src/depiction/persistence/format_ome_tiff.py
@@ -1,4 +1,5 @@
 # TODO figure out if format specific exporters should actually be moved to a different path
+
 # TODO figure out the ideal extension i.e. tif vs tiff!
 from pathlib import Path
 from typing import Any
@@ -6,6 +7,8 @@ from typing import Any
 import ome_types
 import tifffile
 import xarray
+from bioio.writers import OmeTiffWriter
+from bioio_base.types import PhysicalPixelSizes
 
 from depiction.persistence.pixel_size import PixelSize
 
@@ -22,55 +25,12 @@ class OmeTiff:
         The image must have the dimensions c, y, x and an attribute "pixel_size" with the pixel size information.
         """
         channel_names = list(image.coords["c"].values)
-        image_export = image.expand_dims("s").transpose("c", "y", "x", "s")
-
-        # export
-        tifffile.imwrite(
-            path,
-            image_export.data,
-            metadata={"axes": "CYXS", "channel_names": channel_names},
+        image_export = image.transpose("c", "y", "x")
+        ps_x, ps_y = image.attrs["pixel_size"].size_x, image.attrs["pixel_size"].size_y
+        pixel_sizes = PhysicalPixelSizes(Z=None, Y=ps_y, X=ps_x)
+        OmeTiffWriter.save(
+            image_export.data, path, channel_names=channel_names, physical_pixel_sizes=[pixel_sizes], dim_order="CYX"
         )
-
-        """
-        pixel_size = image.attrs["pixel_size"]
-        #metadata = {
-        #    **cls._get_image_resolution_metadata(pixel_size),
-        #    **cls._get_channel_metadata(channel_names),
-        #}
-
-        # add storage dimensions for the OME-TIFF format
-        #image_export = image.expand_dims("s").transpose("c", "y", "x", "s")
-        image_export = image.expand_dims(["z", "t"]).transpose( "x", "y", "z", "c", "t")
-        #image_export = image.expand_dims("s").transpose("c", "y", "x", "c", "t")
-
-        # create OME-XML metadata
-        pixels_data = ome_types.model.Pixels(
-            # TODO check "PixelType"
-            type="float",
-            size_c=image_export.sizes["c"],
-            size_t=1,
-            size_z=1,
-            size_x=image_export.sizes["x"],
-            size_y=image_export.sizes["y"],
-            dimension_order="XYZCT",
-            channels=[{"name": name} for name in channel_names],
-            physical_size_x=pixel_size.size_x,
-            physical_size_y=pixel_size.size_y,
-        )
-        image_data = ome_types.model.Image(pixels=pixels_data)
-        ome_data = ome_types.model.OME(images=[image_data])
-        ome_xml = ome_types.to_xml(ome_data)
-
-        # export
-        tifffile.imwrite(
-            path,
-            image_export.data.T,
-            description=ome_xml,
-            metadata=None,
-            ome=True,
-            photometric="minisblack",
-            #metadata={"axes": "CYXS", **cls._get_channel_metadata(channel_names)},
-        )"""
 
     @classmethod
     def read(cls, path: Path) -> xarray.DataArray:

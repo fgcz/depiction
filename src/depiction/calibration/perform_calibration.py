dd66b049f755d41a395af8f7633d11081f674a44
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-10-24 13:53:54 +0200

    extract ApplyModels for easier testing

--- src/depiction/calibration/perform_calibration.py
+++ src/depiction/calibration/perform_calibration.py
@@ -44,20 +44,8 @@ class PerformCalibration:
         self._write_data_array(model_coefs, group="model_coefs")
 
         logger.info("Applying models...")
-        self._apply_all_models(read_file=read_full, write_file=write_file, all_model_coefs=model_coefs)
-
-    def _apply_all_models(
-        self, read_file: GenericReadFile, write_file: GenericWriteFile, all_model_coefs: MultiChannelImage
-    ) -> None:
-        write_parallel = WriteSpectraParallel.from_config(self._parallel_config)
-        write_parallel.map_chunked_to_file(
-            read_file=read_file,
-            write_file=write_file,
-            operation=self._calibrate_spectra,
-            bind_args=dict(
-                calibration=self._calibration,
-                all_model_coefs=all_model_coefs,
-            ),
+        ApplyModels(self._calibration, self._parallel_config).write_to_file(
+            read_file=read_full, write_file=write_file, all_model_coefs=model_coefs
         )
 
     def _write_data_array(self, image: MultiChannelImage, group: str) -> None:
@@ -65,23 +53,6 @@ class PerformCalibration:
         if not self._coefficient_output_file:
             return
         image.write_hdf5(path=self._coefficient_output_file, mode="a", group=group)
-
-    @staticmethod
-    def _calibrate_spectra(
-        reader: GenericReader,
-        spectra_indices: list[int],
-        writer: GenericWriter,
-        calibration: CalibrationMethod,
-        all_model_coefs: MultiChannelImage,
-    ) -> None:
-        for spectrum_id in spectra_indices:
-            # TODO sanity check the usage of i as spectrum_id (i.e. check the coords!)
-            mz_arr, int_arr, coords = reader.get_spectrum_with_coords(spectrum_id)
-            features = all_model_coefs.data_flat.isel(i=spectrum_id)
-            calib_mz_arr, calib_int_arr = calibration.apply_spectrum_model(
-                spectrum_mz_arr=mz_arr, spectrum_int_arr=int_arr, model_coef=features
-            )
-            writer.add_spectrum(calib_mz_arr, calib_int_arr, coords)
 
 
 class ExtractFeatures:
@@ -150,3 +121,40 @@ class FitModels:
             model_coef = self._calibration.fit_spectrum_model(features=features)
             collect.append(model_coef)
         return xarray.concat(collect, dim="i")
+
+
+class ApplyModels:
+    def __init__(self, calibration: CalibrationMethod, parallel_config: ParallelConfig) -> None:
+        self._calibration = calibration
+        self._parallel_config = parallel_config
+
+    def write_to_file(
+        self, read_file: GenericReadFile, write_file: GenericWriteFile, all_model_coefs: MultiChannelImage
+    ) -> None:
+        write_parallel = WriteSpectraParallel.from_config(self._parallel_config)
+        write_parallel.map_chunked_to_file(
+            read_file=read_file,
+            write_file=write_file,
+            operation=self.calibrate_spectra,
+            bind_args=dict(
+                calibration=self._calibration,
+                all_model_coefs=all_model_coefs,
+            ),
+        )
+
+    @staticmethod
+    def calibrate_spectra(
+        reader: GenericReader,
+        spectra_indices: list[int],
+        writer: GenericWriter,
+        calibration: CalibrationMethod,
+        all_model_coefs: MultiChannelImage,
+    ) -> None:
+        for spectrum_id in spectra_indices:
+            # TODO sanity check the usage of i as spectrum_id (i.e. check the coords!)
+            mz_arr, int_arr, coords = reader.get_spectrum_with_coords(spectrum_id)
+            features = all_model_coefs.data_flat.isel(i=spectrum_id)
+            calib_mz_arr, calib_int_arr = calibration.apply_spectrum_model(
+                spectrum_mz_arr=mz_arr, spectrum_int_arr=int_arr, model_coef=features
+            )
+            writer.add_spectrum(calib_mz_arr, calib_int_arr, coords)
